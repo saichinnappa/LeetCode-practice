f8e6a6e7d8e4f7c49115feea8164a96882fa0c1b
saichinnappa <chinnappa.sai@gmail.com>
2021-12-31 11:47:42 -0600

    Time: 1 ms (80.71%), Space: 44.5 MB (14.81%) - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,25 +1,30 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
      
-        int k = 0;
+//         int k = 0;
+//         boolean found = false;
+
+//         for(int i = 0; i < nums.length - 1; i++){
+//             if(k != 0 && nums[i] != nums[i + 1]){
+//                 nums[k] = nums[i + 1];
+//                 k++;
+//             } else if(nums[i] == nums[i+1]){
+//                 if(!found){
+//                     k = i + 1;
+//                     found = true;
+//                 }
+//             }
+//         }
+//         return k == 0 ? nums.length : k;
         
-        for(int i = 0; i< nums.length - 1; i++){
-            if(nums[i] == nums[i+1]){
-                k = i + 1;
-                break;
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
             }
         }
         
-        if(k != 0){
-            for(int i = k; i < nums.length - 1; i++){
-                if(nums[i] != nums[i + 1]){
-                    nums[k] = nums[i + 1];
-                    k++;
-                }
-            }
-        }
-        
-        return k == 0 ? nums.length : k;
-        
+        return i + 1;
     }
 }
