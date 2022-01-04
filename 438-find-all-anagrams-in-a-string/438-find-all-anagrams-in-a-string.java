e1dc364a9555c402ce9552a69f3331afe09bcf2d
saichinnappa <chinnappa.sai@gmail.com>
2022-01-04 07:26:10 -0600

    Time: 25 ms (36.54%), Space: 44.7 MB (18.36%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,32 +1,52 @@
 class Solution {
-  public List<Integer> findAnagrams(String s, String p) {
-    int ns = s.length(), np = p.length();
-    if (ns < np) return new ArrayList();
-
-    int [] pCount = new int[26];
-    int [] sCount = new int[26];
-    // build reference array using string p
-    for (char ch : p.toCharArray()) {
-      pCount[(int)(ch - 'a')]++;
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        if(p.length() > s.length())
+            return new ArrayList();
+        
+        List<Integer> result = new ArrayList();
+        char[] sMap = new char[26];
+        char[] pMap = new char[26];
+        
+        for(int i = 0; i< p.length(); i++){
+            pMap[p.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+        
+            
+            sMap[s.charAt(i) - 'a']++;
+            
+            if(i >= p.length()){
+                sMap[s.charAt(i - p.length()) - 'a']--;
+            }
+            
+            // sMap[s.charAt(i + p.length()) -'a']++;
+            // sMap[s.charAt(i) - 'a']--;
+            
+            if(Arrays.equals(sMap, pMap)){
+                result.add(i - p.length() + 1);
+            }
+            
+        }
+        
+        // if(matches(pMap, sMap)){
+        //     result.add(s.length() - p.length());
+        // }
+        
+        
+        return result;
+        
+        
     }
-
-    List<Integer> output = new ArrayList();
-    // sliding window on the string s
-    for (int i = 0; i < ns; ++i) {
-      // add one more letter 
-      // on the right side of the window
-      sCount[(int)(s.charAt(i) - 'a')]++;
-      // remove one letter 
-      // from the left side of the window
-      if (i >= np) {
-        sCount[(int)(s.charAt(i - np) - 'a')]--;
-      }
-      // compare array in the sliding window
-      // with the reference array
-      if (Arrays.equals(pCount, sCount)) {
-        output.add(i - np + 1);
-      }
+    
+    
+    boolean matches(char[] pMap, char[] sMap){
+        for(int i = 0; i< pMap.length; i++){
+            if(pMap[i] != sMap[i])
+                return false;
+        }
+        return true;
     }
-    return output;
-  }
+    
 }
