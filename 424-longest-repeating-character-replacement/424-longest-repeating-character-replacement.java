85cbb53f18f60d8da97e9897dee4dcea31c75b84
saichinnappa <chinnappa.sai@gmail.com>
2022-01-03 18:46:59 -0600

    Time: 16 ms (36.59%), Space: 40.1 MB (13.47%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -4,14 +4,17 @@ class Solution {
         //     return 0;
         
         char[] arr = s.toCharArray();
+        char[] count = new char[26];
         Map<Character, Integer> map = new HashMap();
         int result = 0, maxCount = 0, startWindow = 0;
         
         for(int i = 0; i < arr.length; i++){
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            maxCount = Math.max(maxCount, map.get(arr[i]));
+            count[arr[i] - 'A']++;
+            // map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            maxCount = Math.max(maxCount, count[arr[i] - 'A']);
             if((i - startWindow + 1 - maxCount) > k){
-                map.put(arr[startWindow], map.get(arr[startWindow]) - 1);
+                count[arr[startWindow] - 'A']--;
+                // map.put(arr[startWindow], map.get(arr[startWindow]) - 1);
                 startWindow++;
             }
             result = Math.max(result, i - startWindow + 1);
