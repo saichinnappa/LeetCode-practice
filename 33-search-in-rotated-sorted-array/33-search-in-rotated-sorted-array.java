f1c27455d0be76a67e0dc78d8e66b3bef98cf4c0
saichinnappa <chinnappa.sai@gmail.com>
2022-01-12 11:29:24 -0600

    Time: 30 ms (5.34%), Space: 39.7 MB (7.58%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,57 +1,64 @@
 class Solution {
+    int target = 0;
     public int search(int[] nums, int target) {
-        if(nums.length == 0)
-            return -1;
-        if (nums.length == 1)
-            return nums[0] == target ? 0 : -1;
-        int result = -1;
-        int length = nums.length - 1;
-        int rIdx = rotatedIndex(nums, 0, length);
-        if(rIdx == 0)
-            return binarySearch(nums, target, 0, length);
-        if(nums[rIdx] == target)
-            return rIdx;
-        else if(target > nums[length]){
-            return binarySearch(nums, target, 0, rIdx - 1);
-        } else{
-            return binarySearch(nums, target, rIdx + 1, length);
+        this.target = target;
+        
+        if(nums.length == 1){
+            if(target == nums[0])
+                return 0;
+            else
+                return -1;
         }
         
-    }
-    
-    int binarySearch(int[] nums, int target, int start, int end){
+        int length = nums.length - 1;
+        int rIdx = findRotatedIndex(nums, 0, length);
+        System.out.println("Rotated Index: "+ rIdx);
+        if(nums[rIdx] == target)
+            return rIdx;
+        
+        if(rIdx == 0)
+            return binarySearch(nums, 0 , length);
+        
+        if(target <= nums[length]){
+            return binarySearch(nums, rIdx + 1, length);
+        } else{
+            return binarySearch(nums, 0, rIdx - 1);
+        }
+            
+}
+
+    private int binarySearch(int[] nums, int start, int end){
         while(start <= end){
-            int mid = (start + end)/2;
-            if(nums[mid] == target)
+            int mid = (start + end) /2;
+            // System.out.println(mid);
+            if(nums[mid]  == target)
                 return mid;
-            else if(nums[mid] < target){
+            if(nums[mid] < target){
                 start = mid + 1;
-            } else{
-                end = mid -1;
+            } else if (nums[mid] > target){
+                end = mid - 1;
             }
         }
         return -1;
     }
     
-    int rotatedIndex(int[] nums, int start, int end){
-        
-         if (nums[start] < nums[end])
+    
+    
+    int findRotatedIndex(int[] nums, int start, int end){
+        if(nums[start] < nums[end])
             return 0;
+
         while(start <= end){
-            
             int mid = (start + end) /2;
+            System.out.println(mid);
             if(nums[mid] > nums[mid + 1])
                 return mid + 1;
-            else{
-                if(nums[mid] < nums[start])
-                    end = mid -  1;
-                else
-                    start = mid + 1;
-            }
+            else if (nums[mid] >= nums[start])
+                start = mid + 1;
+            else if (nums[mid] < nums[start])
+                end = mid - 1;
         }
-        return 0;
+
+        return -1;
     }
-    
-    
-    
 }
