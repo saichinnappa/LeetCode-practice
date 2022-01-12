0e4ec2138b28f24be63ff95e8992f1894d9b6803
saichinnappa <chinnappa.sai@gmail.com>
2022-01-11 18:55:54 -0600

    Time: 5 ms (33.22%), Space: 52.4 MB (14.64%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,55 +1,28 @@
-// class Solution {
-    
-//     List<List<Integer>> result = new ArrayList();
-//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-//                 if(graph == null || graph.length == 0) return null;
-//         int source = 0;
-//         int dest = graph.length - 1;
-//         List<Integer> list = new ArrayList();
-//         list.add(source);
-//         dfs(graph, source, dest, list);
-        
-//         return result;
-//     }
-    
-//     void dfs(int[][] graph, int src, int dest, List<Integer> list){
-//         if(src == dest){
-//             result.add(new ArrayList<>(list));
-//             return;
-//         }
-//         int[] edges = graph[src];
-
-//         for(int edge : edges){
-//             list.add(edge);
-//             dfs(graph, edge, dest, list);
-//             list.remove(list.size() - 1);
-//         }
-//     }
-// }
-
 class Solution {
-    List<List<Integer>> results = new ArrayList();
-    int destination;
+    
+    List<List<Integer>> result = new ArrayList();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        if(graph == null || graph.length == 0) return null;
-        destination = graph.length -1 ;
-        traverse(0, destination, new ArrayList<>(), graph);
-        return results;
+                if(graph == null || graph.length == 0) return null;
+        int source = 0;
+        int dest = graph.length - 1;
+        List<Integer> list = new ArrayList();
+        list.add(source);
+        dfs(graph, source, dest, list);
         
+        return result;
     }
     
-    public void traverse(int source, int target, List<Integer> path, int[][] graph){
-        path.add(source);
-        if(source == target){
-            results.add(new ArrayList<>(path));
+    void dfs(int[][] graph, int src, int dest, List<Integer> list){
+        if(src == dest){
+            result.add(new ArrayList<>(list));
             return;
         }
-        
-        for(int i = 0; i< graph[source].length; i++){
-           traverse(graph[source][i], target, path, graph);
-            path.remove(path.size() - 1);
+        int[] edges = graph[src];
+
+        for(int edge : edges){
+            list.add(edge);
+            dfs(graph, edge, dest, list);
+            list.remove(list.size() - 1);
         }
-        
     }
-    
 }
