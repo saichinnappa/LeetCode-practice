77a01943b32dbd8e5474b0f2724be63a92297a77
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-01-24 18:52:56 -0600

    Time: 15 ms (36.15%), Space: 41.2 MB (40.74%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,39 +1,40 @@
 class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if(s.length() <= 1) return s.length();
-        
-//         int result = 0;
-//         int start = 0;
-        
-//         Map<Character, Integer> map = new HashMap();
-//         for(int i = 0; i< s.toCharArray().length; i ++){
-//             if(map.containsKey(s.charAt(i))){
-//                 if(start <= map.get(s.charAt(i)))
-//                     start = map.get(s.charAt(i)) + 1;
-//             }
-//             map.put(s.charAt(i), i);
-//             result = Math.max(result, i - start + 1);
-                
-//         }
-//         return result;
-//     }
-    
     public int lengthOfLongestSubstring(String s) {
+        if(s.length() <= 1) return s.length();
         
-        Map<Character, Integer> map= new HashMap<>();
-        int start=0, len=0;
+        int result = 0;
+        int start = 0;
         
-        // abba
-        for(int i=0; i<s.length(); i++) {
+        Map<Character, Integer> map = new HashMap();
+        for(int i = 0; i< s.length(); i ++){
             char c = s.charAt(i);
-            if (map.containsKey(c)) {
-                if (map.get(c) >= start) 
+            if(map.containsKey(c)){
+                if(start <= map.get(c))
                     start = map.get(c) + 1;
             }
-            len = Math.max(len, i-start+1);
             map.put(c, i);
+            result = Math.max(result, i - start + 1);
+                
         }
-        
-        return len;
+        return result;
     }
+    
+//     public int lengthOfLongestSubstring(String s) {
+        
+//         Map<Character, Integer> map= new HashMap<>();
+//         int start=0, len=0;
+        
+//         // abba
+//         for(int i=0; i<s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (map.containsKey(c)) {
+//                 if (map.get(c) >= start) 
+//                     start = map.get(c) + 1;
+//             }
+//             len = Math.max(len, i-start+1);
+//             map.put(c, i);
+//         }
+        
+//         return len;
+//     }
 }
