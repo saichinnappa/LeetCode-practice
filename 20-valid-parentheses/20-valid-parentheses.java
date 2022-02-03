b52d74792788d5d6dc18d38a9f82d0e384003595
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-02 19:55:16 -0600

    Time: 2 ms (63.55%), Space: 40.1 MB (10.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,74 +1,27 @@
-// class Solution {
-//     public boolean isValid(String s) {
-//         if(s.length() == 0) return true;
-        
-//         Map<Character, Character> map = new HashMap();
-//         map.put(')', '(');
-//         map.put(']', '[');
-//         map.put('}', '{');
-        
-//         char[] input = s.toCharArray();
-//         Deque<Character> stack = new ArrayDeque();
-        
-//         for(int i = 0; i < input.length; i++){
-//             if(!map.containsKey(input[i]))
-//                 stack.push(input[i]);
-//             else{
-//                 if(stack.isEmpty()){
-//                     return false;
-//                 } else{
-//                     char top = stack.pop();
-//                     if(top != map.get(input[i])){
-//                         return false;
-//                    }    
-//                 }
-                
-//             }
-//         }
-        
-//         return stack.isEmpty();
-        
-//     }
-// }
-
 class Solution {
-
-  // Hash table that takes care of the mappings.
-  private HashMap<Character, Character> mappings;
-
-  // Initialize hash map with mappings. This simply makes the code easier to read.
-  public Solution() {
-    this.mappings = new HashMap<Character, Character>();
-    this.mappings.put(')', '(');
-    this.mappings.put('}', '{');
-    this.mappings.put(']', '[');
-  }
-
-  public boolean isValid(String s) {
-
-    // Initialize a stack to be used in the algorithm.
-    Stack<Character> stack = new Stack<Character>();
-
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-
-      // If the current character is a closing bracket.
-      if (this.mappings.containsKey(c)) {
-
-        // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
-        char topElement = stack.empty() ? '#' : stack.pop();
-
-        // If the mapping for this bracket doesn't match the stack's top element, return false.
-        if (topElement != this.mappings.get(c)) {
-          return false;
+    public boolean isValid(String s) {
+        if(s.length() <=1)
+            return false;
+        Map<Character, Character> map = new HashMap();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        
+        Stack<Character> stack = new Stack();
+        stack.push(s.charAt(0));
+        
+        for(int i = 1; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                if(stack.size() > 0 && map.get(c) == stack.peek()){
+                    stack.pop();
+                } else{
+                    return false;
+                }
+            } else{
+                stack.push(c);
+            }
         }
-      } else {
-        // If it was an opening bracket, push to the stack.
-        stack.push(c);
-      }
+        return stack.size() == 0;
     }
-
-    // If the stack still contains elements, then it is an invalid expression.
-    return stack.isEmpty();
-  }
 }
