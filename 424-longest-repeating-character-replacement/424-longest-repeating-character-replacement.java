26e903388f1bf4ddf3bb7bbc0a2a4d2492128980
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-02 19:08:53 -0600

    Time: 189 ms (5.05%), Space: 49 MB (5.53%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,21 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int startWindow = 0;
-        char[] count = new char[26];
-        int result = 0;
+        
+        int max = 0;
+        char[] arr = s.toCharArray();
+        int[] count = new int[26];
+        int start = 0;
         int maxCount = 0;
-        for(int i = 0; i< s.length(); i++){
-            char c = s.charAt(i);
-            count[c - 'A']++;
-            maxCount = Math.max(maxCount, count[c  - 'A']);
-            int uniqueChars = (i - startWindow + 1) - maxCount;
+        
+        for(int i = 0; i < arr.length; i++){
             
-            if(uniqueChars > k){
-                count[s.charAt(startWindow) - 'A']--;
-                startWindow++;
+            maxCount = Math.max(maxCount, ++count[arr[i] - 'A']); //increment the count of the current character in the counter
+            if((i - start + 1) - maxCount <= k){
+                System.out.println(arr[i]+"->"+count[arr[i]-'A']);
+                max = Math.max(max, i - start + 1);
+            } else{
+          
+                count[arr[start] - 'A']--;
+                      start++;
             }
-            result = Math.max(result, i - startWindow + 1);
+            
+            
         }
-        return result;
+        
+        return max;
     }
 }
