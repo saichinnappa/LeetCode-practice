3c5a23853ecab67e5afa91a4d85a9a7e9284c39e
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-03 09:26:45 -0600

    Time: 2 ms (7.43%), Space: 42.6 MB (6.80%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,36 +1,3 @@
-// /**
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-//  */
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//         if(list1 == null) return list2;
-//         if(list2 == null) return list1;
-        
-//         ListNode result = new ListNode(-1);
-//         ListNode x = result;
-//         while(list1 != null && list2 != null){
-//             if(list1.val <= list2.val){
-//                 result.next = new ListNode(list1.val);
-//                 list1 = list1.next;
-//             } else if (list1.val > list2.val){
-//                 result.next = new ListNode(list2.val);
-//                 list2 = list2.next;
-//             }
-//             result = result.next;
-//         }
-        
-//         result.next = (list1 == null) ? list2 : list1;
-//         return x.next;
-//     }
-// }
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -43,23 +10,32 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-        
-        ListNode dummy = new ListNode(0);
-        ListNode result = dummy;
-        
+        ListNode result = new ListNode(101);
+        ListNode result1 = result;
         while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                dummy.next = new ListNode(list1.val);
+            if(list1.val < list2.val){
+                result.next = new ListNode(list1.val);
                 list1 = list1.next;
-            } else{
-                dummy.next = new ListNode(list2.val);
+            } else if (list2.val < list1.val){
+                result.next = new ListNode(list2.val);
                 list2 = list2.next;
+            } else{
+                result.next = new ListNode(list1.val);
+                list1 = list1.next;
+                result = result.next;
+                result.next = new ListNode(list2.val);
+                list2 = list2.next;
+            
             }
-            dummy = dummy.next;
+            result = result.next;
         }
-        dummy.next = (list1 == null) ? list2 : list1;
-        return result.next;        
+        
+        if(list1 == null){
+            result.next = list2;
+        } else if(list2 == null){
+            result.next = list1;
+        }
+        
+        return result1.next;
     }
 }
