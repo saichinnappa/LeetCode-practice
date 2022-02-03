20c0b1c498907a8eeac46371c036ae356ba4cf99
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-03 10:58:46 -0600

    Time: 0 ms (100.00%), Space: 40.2 MB (9.14%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -13,27 +13,48 @@ class Solution {
         
         // APPROACH 1
         // First count all the nodes and then remove (total - n + 1)
+//         ListNode dummy = new ListNode(-1);
+//         dummy.next = head;
+//         ListNode first = head;
+//         int count = 0;
+        
+//         while(first != null){
+//             count++;
+//             first = first.next;
+//         }
+        
+//         first = dummy;
+//         count = count - n;
+        
+//         while(count > 0){
+//             count--;
+//             first = first.next;
+//         }
+        
+//         first.next = first.next.next;
+        
+//         return dummy.next;
+        
+        // APPROACH 2
+        //Two pointers
+        // slow keep running at one node at a time, whereas fast runs n times so that slow and fast are n distance apart at any given time.
+        
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode first = head;
-        int count = 0;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
         
-        while(first != null){
-            count++;
-            first = first.next;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
         }
         
-        first = dummy;
-        count = count - n;
-        
-        while(count > 0){
-            count--;
-            first = first.next;
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
         }
         
-        first.next = first.next.next;
+        slow.next = slow.next.next;
         
         return dummy.next;
-        
     }
 }
