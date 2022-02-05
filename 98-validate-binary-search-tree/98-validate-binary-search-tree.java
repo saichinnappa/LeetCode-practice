98d128003c244b5717f340d3c1c0313f4b071b8c
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-05 09:22:06 -0600

    Time: 3 ms (12.79%), Space: 43.9 MB (6.91%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public boolean isValidBST(TreeNode root) {
+        
+        //Recursive
+        // return validate(root, null, null);
+        
+        //Iterative using inorder property
+        Stack<TreeNode> stack = new Stack();
+        Integer prev = null;
+        
+        while(!stack.isEmpty() || root != null){
+            
+            //starting from root push all the left childs to stack
+            //Once the left of a given root is popped, will be pushing right child to stack.
+            while(root != null){
+                stack.push(root);
+                root = root.right;
+            }
+            
+            // get the last left child from the stack
+            TreeNode top = stack.pop();
+            //compare with prev element, and this should always be greater (which means root val), if not its not a BST return false;
+            if(prev != null && top.val >= prev){
+                return false;
+            }
+            
+            //copy popped left value to prev and then move root to right of the popped element.
+            prev = top.val;
+            root = top.left;
+        }
+        return true;
+        
+    }
+}
