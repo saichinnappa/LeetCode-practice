f7d141711a447a237cf0fb8bb7bf176f0ee4089d
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-05 09:57:37 -0600

    Time: 1 ms (55.81%), Space: 43.6 MB (10.16%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,23 +14,44 @@
  * }
  */
 class Solution {
+    
+    TreeNode prev  = null;
+    boolean isValid = true;
     public boolean isValidBST(TreeNode root) {
-        TreeNode prev = null;
-        Deque<TreeNode> stack = new ArrayDeque();
-        while(!stack.isEmpty() || root != null){
-            while(root != null){
-                stack.push(root);
-                root = root.left;
-            }
+        
+//         // Approach 1: Iterative
+//         TreeNode prev = null;
+//         Deque<TreeNode> stack = new ArrayDeque();
+//         while(!stack.isEmpty() || root != null){
+//             while(root != null){
+//                 stack.push(root);
+//                 root = root.left;
+//             }
 
-            TreeNode top = stack.pop();
-            if(prev != null && prev.val >= top.val){
-                return false;
-            }
+//             TreeNode top = stack.pop();
+//             if(prev != null && prev.val >= top.val){
+//                 return false;
+//             }
 
-            prev = top;
-            root = top.right;   
+//             prev = top;
+//             root = top.right;   
+//         }
+//         return true;
+        
+        // Approach 2: Recursive
+        recursive(root);
+        return isValid;
+    }
+    
+    void recursive(TreeNode root){
+        if(root == null)
+            return;
+        recursive(root.left);
+        if(prev != null && prev.val >= root.val){
+            isValid = false;
+            return;
         }
-        return true;
+        prev = root;
+        recursive(root.right);
     }
 }
