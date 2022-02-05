c95bbf3080b62bb99f31f6cb48f7e0de359cbfa3
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-05 09:52:02 -0600

    Time: 38 ms (5.07%), Space: 44.9 MB (5.02%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,39 +14,24 @@
  * }
  */
 class Solution {
-   public boolean isValidBST(TreeNode root) {
-        
-        //Recursive
-        // return validate(root, null, null);
-        
-        //Iterative using inorder property
-        Stack<TreeNode> stack = new Stack();
-        Integer prev = null;
-        
+    public boolean isValidBST(TreeNode root) {
+        TreeNode prev = null;
+        Deque<TreeNode> stack = new ArrayDeque();
         while(!stack.isEmpty() || root != null){
-            
-            //starting from root push all the left childs to stack
-            //Once the left of a given root is popped, will be pushing right child to stack.
             while(root != null){
                 stack.push(root);
                 root = root.left;
             }
-            
-            // get the last left child from the stack
+
             TreeNode top = stack.pop();
-            //compare with prev element, and this should always be greater (which means root val), if not its not a BST return false;
-            if(prev != null){
-                System.out.println("Comparing: prev->"+prev+" | "+ top.val);
-            }
-            if(prev != null && top.val <= prev){
+            System.out.println(top.val);
+            if(prev != null && prev.val >= top.val){
                 return false;
             }
-            
-            //copy popped left value to prev and then move root to right of the popped element.
-            prev = top.val;
-            root = top.right;
+
+            prev = top;
+            root = top.right;   
         }
         return true;
-        
     }
 }
