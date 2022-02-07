335bc351da61108836b5aaf5f4e0cf9b9dcb5472
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-07 13:02:44 -0600

    Time: 1 ms (79.22%), Space: 42.5 MB (13.67%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,3 +1,48 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     List<List<Integer>> result = new ArrayList();
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+//         if(root == null)
+//             return result;
+        
+//         Queue<TreeNode> queue = new ArrayDeque();
+//         queue.offer(root);
+        
+//         while(!queue.isEmpty()){
+//             List<Integer> list = new ArrayList();
+//             int size = queue.size();
+//             while(size > 0){
+//                 TreeNode top = queue.poll();
+//                 list.add(top.val);
+//                 if(top.left != null){
+//                     queue.offer(top.left);
+//                 }
+//                 if(top.right != null){
+//                     queue.offer(top.right);
+//                 }
+//                 size--;
+//             }
+//             result.add(list);
+//         }
+//         return result;
+//     }
+// }
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,9 +59,9 @@
  * }
  */
 class Solution {
+    
     List<List<Integer>> result = new ArrayList();
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
         if(root == null)
             return result;
         
@@ -26,19 +71,22 @@ class Solution {
         while(!queue.isEmpty()){
             List<Integer> list = new ArrayList();
             int size = queue.size();
-            while(size > 0){
-                TreeNode top = queue.poll();
-                list.add(top.val);
-                if(top.left != null){
-                    queue.offer(top.left);
-                }
-                if(top.right != null){
-                    queue.offer(top.right);
-                }
+            while(size != 0){
+                TreeNode first = queue.poll();
+                list.add(first.val);
+                if(first.left != null)
+                    queue.offer(first.left);
+                if(first.right != null)
+                    queue.offer(first.right);
                 size--;
             }
-            result.add(list);
+            if(list.size() > 0)
+                result.add(list);
+            
         }
+        
+        
+        
         return result;
     }
 }
