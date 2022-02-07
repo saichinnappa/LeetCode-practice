3218007519af3c4362d7442671a8a83ebec073fb
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-07 13:03:26 -0600

    Time: 1 ms (79.22%), Space: 43.7 MB (5.78%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,48 +1,3 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
-//     List<List<Integer>> result = new ArrayList();
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-        
-//         if(root == null)
-//             return result;
-        
-//         Queue<TreeNode> queue = new ArrayDeque();
-//         queue.offer(root);
-        
-//         while(!queue.isEmpty()){
-//             List<Integer> list = new ArrayList();
-//             int size = queue.size();
-//             while(size > 0){
-//                 TreeNode top = queue.poll();
-//                 list.add(top.val);
-//                 if(top.left != null){
-//                     queue.offer(top.left);
-//                 }
-//                 if(top.right != null){
-//                     queue.offer(top.right);
-//                 }
-//                 size--;
-//             }
-//             result.add(list);
-//         }
-//         return result;
-//     }
-// }
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -59,9 +14,9 @@
  * }
  */
 class Solution {
-    
     List<List<Integer>> result = new ArrayList();
     public List<List<Integer>> levelOrder(TreeNode root) {
+        
         if(root == null)
             return result;
         
@@ -71,22 +26,67 @@ class Solution {
         while(!queue.isEmpty()){
             List<Integer> list = new ArrayList();
             int size = queue.size();
-            while(size != 0){
-                TreeNode first = queue.poll();
-                list.add(first.val);
-                if(first.left != null)
-                    queue.offer(first.left);
-                if(first.right != null)
-                    queue.offer(first.right);
+            while(size > 0){
+                TreeNode top = queue.poll();
+                list.add(top.val);
+                if(top.left != null){
+                    queue.offer(top.left);
+                }
+                if(top.right != null){
+                    queue.offer(top.right);
+                }
                 size--;
             }
             if(list.size() > 0)
                 result.add(list);
-            
         }
-        
-        
-        
         return result;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+    
+//     List<List<Integer>> result = new ArrayList();
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         if(root == null)
+//             return result;
+        
+//         Queue<TreeNode> queue = new ArrayDeque();
+//         queue.offer(root);
+        
+//         while(!queue.isEmpty()){
+//             List<Integer> list = new ArrayList();
+//             int size = queue.size();
+//             while(size != 0){
+//                 TreeNode first = queue.poll();
+//                 list.add(first.val);
+//                 if(first.left != null)
+//                     queue.offer(first.left);
+//                 if(first.right != null)
+//                     queue.offer(first.right);
+//                 size--;
+//             }
+
+            
+//         }
+        
+        
+        
+//         return result;
+//     }
+// }
