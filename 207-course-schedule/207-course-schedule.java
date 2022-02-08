96147fffefa18d09ab8b0aca4f33d2f0acdca6f3
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-07 18:01:51 -0600

    Time: 8 ms (55.53%), Space: 49.1 MB (11.94%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,62 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // APPROACH 1: BFS
+        Queue<Integer> queue = new ArrayDeque();
+        int[] courseCounter = new int[numCourses];
+        Map<Integer, List<Integer>> graph = new HashMap();
+        
+        // STEP 1: Build Adjacency list aka graph
+        for(int[] pre: prerequisites){
+            graph.putIfAbsent(pre[1], new ArrayList());
+            graph.get(pre[1]).add(pre[0]);
+            courseCounter[pre[0]]++; //using this counter to determine the courses which doesn't have a pre-req.
+        }
+        
+         for(int i = 0; i < courseCounter.length; i++){
+            if(courseCounter[i] == 0){ // push all the courses which doesn't have a pre-req to the queue to begin processing.
+                queue.offer(i);
+            }
+        }
+        boolean[] visited = new boolean[numCourses];
+        
+        while(!queue.isEmpty()){
+            int first = queue.poll();
+            List<Integer> dependentCourses = graph.get(first);
+            
+            visited[first] = true;
+            if(dependentCourses != null && dependentCourses.size() > 0){
+            for(int c : dependentCourses){
+                    if(courseCounter[c] > 0){
+                        courseCounter[c]--;
+                    }
+                    if(courseCounter[c] == 0){
+                        queue.offer(c);
+                    }
+            }
+            }
+            
+            
+            
+//             if(dependentCourses != null && dependentCourses.size() > 0){
+                
+//                 for(int i = 0; i < dependentCourses.size(); i++){
+//                     if(visited[dependentCourses.get(i)] && courseCounter[dependentCourses.get(i)] == 0)
+//                         return false;
+//                     visited[dependentCourses.get(i)] = true;
+//                     queue.offer(dependentCourses.get(i));
+//                     courseCounter[dependentCourses.get(i)]--;
+//                 }
+//             }
+            
+        }
+            
+        for(int i = 0; i < courseCounter.length; i++){
+            if(courseCounter[i] > 0)
+                return false;
+                
+        }
+        
+        
+        return true;
+    }
+}
