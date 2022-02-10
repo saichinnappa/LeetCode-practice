185c6ac82422457a7fefba38a2eac49e91c21877
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-09 19:10:15 -0600

    Time: 34 ms (5.06%), Space: 54.1 MB (5.76%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -3,21 +3,21 @@ class Solution {
         if((version1 == null || version1.length() == 0) && (version2 == null || version2.length() == 0))
             return 0;
         
-        String[] v1arr = version1.split("\\.");
-        String[] v2arr = version2.split("\\.");
+        // String[] v1arr = version1.split("\\.");
+        // String[] v2arr = version2.split("\\.");
         
-        int maxLength = (v1arr.length > v2arr.length) ? v1arr.length : v2arr.length;
+        int maxLength = (version1.length() > version2.length()) ? version1.length() : version2.length();
         
         int[] v1 = new int[maxLength];
         int[] v2 = new int[maxLength];
         
-        for(int i = 0; i < v1arr.length; i++){
-            v1[i] = Integer.parseInt(v1arr[i]);
+        for(int i = 0; i < version1.split("\\.").length; i++){
+            v1[i] = Integer.parseInt(version1.split("\\.")[i]);
         }
         
         
-        for(int i = 0; i < v2arr.length; i++){
-            v2[i] = Integer.parseInt(v2arr[i]);
+        for(int i = 0; i < version2.split("\\.").length; i++){
+            v2[i] = Integer.parseInt(version2.split("\\.")[i]);
         }
         
         for(int i = 0; i < maxLength; i++){
@@ -27,16 +27,6 @@ class Solution {
                 return -1;
             }
         }
-        
-        
-        
-        // for(int i = 0; i < v1arr.length; i++){
-        //     for(int j = 0; j < v2arr.length; j++){
-        //         int rev = Integer.parseInt(v1arr[i]);
-        //         int rev2 = Integer.parseInt(v2arr[j]);
-        //         System.out.println(rev+" "+rev2);
-        //     }
-        // }
         
         
         return 0;
