6b398f5afcd716792bd3a41a23409caeb9d1f7f7
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-09 21:08:10 -0600

    Time: 3 ms (48.08%), Space: 57.8 MB (13.16%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -27,17 +27,25 @@ class Solution {
             leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
         }
         
-        rightProduct[nums.length - 1] = 1;
+//         rightProduct[nums.length - 1] = 1;
         
-        for(int i = nums.length -2; i >= 0; i--){
-            rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+//         for(int i = nums.length -2; i >= 0; i--){
+//             rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+//         }
+        
+        int x = 1;
+        
+        for(int i = leftProduct.length -1; i > -1; i--){
+            leftProduct[i] = x * leftProduct[i];
+            x *= nums[i];
         }
         
-        for(int i = 0; i< nums.length; i++){
-            nums[i] = leftProduct[i] * rightProduct[i];
-        }
         
-        return nums;
+        // for(int i = 0; i< nums.length; i++){
+        //     nums[i] = leftProduct[i] * rightProduct[i];
+        // }
+        
+        return leftProduct;
         
     }
 }
