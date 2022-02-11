b0841209abfed38a4ab781d64286db6869b28529
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-11 08:38:37 -0600

    Time: 27 ms (35.69%), Space: 43.6 MB (20.07%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,48 +1,41 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length()) return false;
-    
         Map<Character, Integer> charCount = new HashMap();
-        char[] s1arr = s1.toCharArray();
-        char[] s2arr = s2.toCharArray();
+        int start = 0;
         
-        for(char c : s1arr){
+        for(int i = 0; i < s1.length(); i++){
+            char c = s1.charAt(i);
             if(!charCount.containsKey(c))
                 charCount.put(c, 0);
             charCount.put(c, charCount.get(c) + 1);
         }
-        
         int match = 0;
-        int start = 0;
         
-        for(int end = 0; end < s2arr.length; end++){
-            char c = s2arr[end];
-            if(charCount.get(c) != null){
+        for(int end = 0; end < s2.length(); end++){
+            char c = s2.charAt(end);
+            if(charCount.containsKey(c)){
                 charCount.put(c, charCount.get(c) - 1);
                 if(charCount.get(c) == 0)
                     match++;
             }
-            // System.out.println(match);
+            
             if(match == charCount.size())
                 return true;
             
-            if(end - start + 1 >= s1arr.length){
-                
-                // System.out.println((end - start + 1) +" | "+s2.substring(start, end + 1));
-                char leftChar = s2arr[start];
+            if(end - start + 1 >= s1.length()){
+                char leftChar = s2.charAt(start);
                 start++;
-                if(charCount.get(leftChar) != null){
+                if(charCount.containsKey(leftChar)){
+                    
                     if(charCount.get(leftChar) == 0)
                         match--;
+                    
                     charCount.put(leftChar, charCount.get(leftChar) + 1);
                 }
-                
-               
-                
             }
-            
-            
         }
+        
         return false;
         
     }
