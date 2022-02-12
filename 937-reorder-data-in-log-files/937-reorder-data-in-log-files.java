531a8ef2939fa5263c10312538e46fa4140aaed7
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-11 18:28:26 -0600

    Time: 7 ms (60.31%), Space: 48 MB (16.14%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,55 +1,41 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
         
-        List<String> letterLogs = new ArrayList();
-        for(int i=0; i< logs.length; i++){
-            int x = logs[i].indexOf(" ");
-            if(Character.isLetter(logs[i].charAt(x+1))){
-                letterLogs.add(logs[i]);
-            }
-        }
-        
+        List<String> digitLogs = new ArrayList();
         String[] result = new String[logs.length];
+        int index = 0;
         
-        for(int i = 0; i< letterLogs.size(); i++){
-            for(int j = i + 1; j< letterLogs.size(); j++){
-                // if(i != j){
-                
-                
-                 String[] split1 = letterLogs.get(i).split(" ", 2);
-                 String[] split2 = letterLogs.get(j).split(" ", 2);
-                
-                // String a = letterLogs.get(i).substring(letterLogs.get(i).indexOf(" ")+1, letterLogs.get(i).length());  
-                // String b = letterLogs.get(j).substring(letterLogs.get(j).indexOf(" ")+1, letterLogs.get(j).length());
-                    int aComp = split1[1].compareTo(split2[1]);
-                    if(aComp > 0){
-                        String temp = letterLogs.get(i);
-                        letterLogs.set(i, letterLogs.get(j));
-                        letterLogs.set(j, temp);
-                    } else if (aComp == 0){
-                    // String aPrefix = letterLogs.get(i).substring(0, letterLogs.get(i).indexOf(" "));
-                    // String bPrefix = letterLogs.get(j).substring(0, letterLogs.get(j).indexOf(" "));
-                        if(split1[0].compareTo(split2[0]) > 0){
-                             String temp = letterLogs.get(i);
-                            letterLogs.set(i, letterLogs.get(j));
-                            letterLogs.set(j, temp);
-                        }
-                    }
-                // }
+        for(int i = 0; i < logs.length; i++){
+            if(Character.isDigit(logs[i].split(" ")[1].charAt(0))){
+                digitLogs.add(logs[i]);
+            } else{
+                result[index] = logs[i];
+                index++;
             }
         }
         
-        
-        for(int i = 0; i< logs.length; i++){
-              int x = logs[i].indexOf(" ");
-            if(!Character.isLetter(logs[i].charAt(x+1))){
-                letterLogs.add(logs[i]);
-            }
+        Arrays.sort(result, new TestComparator<String>());
+        // System.out.println(Arrays.toString(result));
+        for(int i = 0;  i < digitLogs.size(); i++){
+            result[index  + i] = digitLogs.get(i);
         }
         
-        
-       result =  letterLogs.toArray(result);
-           return result;
+        return result;
     }
+    
+    
+   
+}
 
+ public class TestComparator<T> implements Comparator<String>{
+        @Override
+        public int compare(String s1, String s2){
+            if(s1 == null || s2 == null) return 0;
+            // System.out.println(s1+" "+ s2);
+            String[] a1 = s1.split(" ", 2);
+            String[] a2 = s2.split(" ", 2);
+            if(a1[1].compareTo(a2[1]) == 0)
+                return a1[0].compareTo(a2[0]);
+            return a1[1].compareTo(a2[1]);
+        }
 }
