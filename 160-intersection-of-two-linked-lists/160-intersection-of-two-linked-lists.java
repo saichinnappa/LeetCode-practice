690284d198c1f80f4502385f5f7d7ec62a9331a6
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-12 10:58:26 -0600

    Time: 1 ms (98.78%), Space: 55.2 MB (13.06%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,29 +1,45 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) {
+//  *         val = x;
+//  *         next = null;
+//  *     }
+//  * }
+//  */
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         Set<ListNode> set = new HashSet();
+//         // ListNode result = null;
+//         while(headA != null){
+//             set.add(headA);
+//             headA = headA.next;
+//         }
+        
+//         while(headB != null){
+//             if(set.contains(headB))
+//                 return headB;
+//             headB = headB.next;
+//         }
+        
+//         return null;
+//     }
+// }
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet();
-        // ListNode result = null;
-        while(headA != null){
-            set.add(headA);
-            headA = headA.next;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
         }
-        
-        while(headB != null){
-            if(set.contains(headB))
-                return headB;
-            headB = headB.next;
-        }
-        
-        return null;
+        return pB;
+        // Note: In the case lists do not intersect, the pointers for A and B
+        // will still line up in the 2nd iteration, just that here won't be
+        // a common node down the list and both will reach their respective ends
+        // at the same time. So pA will be NULL in that case.
     }
 }
