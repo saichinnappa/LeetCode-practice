cefbc6d3e5392845c43d9dc6209d8bd823352760
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-12 19:37:53 -0600

    Time: 0 ms (100.00%), Space: 41.9 MB (6.58%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,20 +2,27 @@ class Solution {
     int sum = 0;
     int[] cache = new int[100];
     public int rob(int[] nums) {
-        int max = 0;
-        Arrays.fill(this.cache, -1);
-        return rob(0, nums);
+        int rob1 = 0, rob2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            int temp = Math.max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
+                
+        }
+        return rob2;
+        // Arrays.fill(this.cache, -1);
+        // return rob(0, nums);
     }
     
-    int rob(int index, int[] nums){
-        if(index >= nums.length)
-            return 0;
+//     int rob(int index, int[] nums){
+//         if(index >= nums.length)
+//             return 0;
         
-        if(cache[index] > -1)
-            return cache[index];
+//         if(cache[index] > -1)
+//             return cache[index];
         
-        int ans = Math.max(rob(index + 1, nums), rob(index + 2, nums) + nums[index]);
-        cache[index] = ans;
-        return ans;
-    }
+//         int ans = Math.max(rob(index + 1, nums), rob(index + 2, nums) + nums[index]);
+//         cache[index] = ans;
+//         return ans;
+    // }
 }
