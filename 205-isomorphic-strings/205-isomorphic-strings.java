109401f5c239091952069d192e6aefe798898308
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-12 21:29:37 -0600

    Time: 15 ms (42.48%), Space: 43.7 MB (8.87%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,38 +1,20 @@
-// class Solution {
-//     public boolean isIsomorphic(String s1, String t1) {
-//         Map<Character,Character> map= new HashMap();
-//         Set<Character> valueSet = new HashSet();
-//         for(int i = 0; i < s1.length(); i++){
-//             char s = s1.charAt(i);
-//             char t = t1.charAt(i);
-//             if(!map.containsKey(s)){
-//                 if(valueSet.contains(t))
-//                     return false;
-//                 map.put(s, t);
-//                 valueSet.add(t);
-            
-//             } else{
-//                 if(map.get(s) != t)
-//                     return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<>();
-        Set<Character> assignedValues = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i)) && map.get(s.charAt(i)) != t.charAt(i)) {
-                return false;
+    public boolean isIsomorphic(String s1, String t1) {
+        Map<Character,Character> map= new HashMap();
+        Set<Character> valueSet = new HashSet();
+        for(int i = 0; i < s1.length(); i++){
+            char s = s1.charAt(i);
+            char t = t1.charAt(i);
+            if(!map.containsKey(s)){
+                if(valueSet.contains(t))
+                    return false;
+                map.put(s, t);
+                valueSet.add(t);
+            
+            } else{
+                if(map.get(s) != t)
+                    return false;
             }
-            if (!map.containsKey(s.charAt(i)) && assignedValues.contains(t.charAt(i))) {
-                return false;
-            }
-            map.put(s.charAt(i), t.charAt(i));
-            assignedValues.add(t.charAt(i));
         }
         return true;
     }
