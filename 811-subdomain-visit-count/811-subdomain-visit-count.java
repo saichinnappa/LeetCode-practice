cdeeaf7eec73e86d8b9ff59efa0c9a092e1897fd
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-13 21:21:03 -0600

    Time: 23 ms (67.27%), Space: 52.5 MB (24.31%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.java
+++ 811-subdomain-visit-count/811-subdomain-visit-count.java
@@ -4,24 +4,20 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i < cpdomains.length; i++){
             String[] arr = cpdomains[i].split(" ");
+            int count = Integer.parseInt(arr[0]);
             
-             map.put(arr[1], map.getOrDefault(arr[1], 0) + Integer.parseInt(arr[0]));
             
-            // if(map.containsKey(arr[1])){
-            //     map.put(arr[1], map.get(arr[1]) + arr[0]);
-            // } else{
-            //     map.put(arr[1], arr[0]);
-            // }
+             map.put(arr[1], map.getOrDefault(arr[1], 0) + count);
             
             String[] subdomain = arr[1].split("\\.");
             if(subdomain.length == 3){
                 String x = subdomain[1]+"."+subdomain[2];
-                map.put(x, map.getOrDefault(x, 0) + Integer.parseInt(arr[0]));
+                map.put(x, map.getOrDefault(x, 0) + count);
                 String y = subdomain[2];
-                map.put(y, map.getOrDefault(y, 0) + Integer.parseInt(arr[0]));
+                map.put(y, map.getOrDefault(y, 0) + count);
             } else{
                 String z = subdomain[1];
-                map.put(z, map.getOrDefault(z, 0) + Integer.parseInt(arr[0]));
+                map.put(z, map.getOrDefault(z, 0) + count);
             }
             
         }
