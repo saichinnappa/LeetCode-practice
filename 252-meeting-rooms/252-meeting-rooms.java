d5c8010730cc3ac0942e67857abed305f4de0928
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-14 11:14:35 -0600

    Time: 5 ms (69.55%), Space: 42 MB (62.26%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         for(int i = 0; i < intervals.length - 1; i++){
-            if(intervals[i][0] <= intervals[i+1][0] && intervals[i][1] > intervals[i + 1][0])
+            if(intervals[i][1] > intervals[i + 1][0])
                 return false;
         }
             return true;
