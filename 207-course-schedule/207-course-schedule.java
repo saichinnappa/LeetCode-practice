fff78112774dd927ff2366eb2ce2ce6903f2e75f
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-17 18:55:00 -0600

    Time: 11 ms (41.75%), Space: 48.9 MB (17.69%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,69 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean result = false;
         
-        // TOPOLOGICAL SORT
-        // Agorithm:
-        /**
-        STEP 1: Build the graph with relation pre-req -> courses
-        ex: [0, 1], [2, 1]
-        Graph:
-        0 -> [1]
-        1 -> []
-        2 -> [1] **/
-        
+        Queue<Integer> queue = new ArrayDeque();
         Map<Integer, List<Integer>> graph = new HashMap();
-        int[] counter = new int[numCourses];
+        int[] courses = new int[numCourses];
         for(int[] pre : prerequisites){
             graph.putIfAbsent(pre[1], new ArrayList());
             graph.get(pre[1]).add(pre[0]);
-            /**STEP 2:
-            Keep track of the course counter to record a course as pre-req. For instance, in the above example 1 is not dependent on any courses, so its count would remain at 0.
-            ex: [1, 0, 1] **/
-            counter[pre[0]]++;
+            courses[pre[0]]++;
         }
         
-        /** STEP 3:
-            Push all the courses which have no dependency to queue.
-        **/
-        Queue<Integer> queue = new LinkedList();
-        for(int i = 0; i < counter.length; i++){
-            if(counter[i] == 0)
+        for(int i = 0; i < courses.length; i++){
+            if(courses[i] == 0)
                 queue.add(i);
         }
         
-        /** STEP 4:
-            While processing queue, decrement the current course requirement in counter with the help of the graph.
-        **/
-        /** STEP 5:
-            Also, keep track of the visited courses, there might be a chance of a loop. So if the same course is being seen then just return before proceeding to other courses. Saves time!
-        **/
-        boolean[] visited = new boolean[numCourses];
+        
         while(!queue.isEmpty()){
             int first = queue.poll();
-            visited[first] = true;
-            List<Integer> courseList = graph.get(first);
-            if(courseList != null && courseList.size() > 0){
-                for(int c : courseList){
-                    if(visited[c])
-                        return false;
-                    counter[c]--;
-                    if(counter[c] == 0)
-                        queue.offer(c);
+            List<Integer> list = graph.get(first);
+            if(list != null && list.size() > 0){
+                for(int i : list){
+                    courses[i]--;
+                    if(courses[i] == 0)
+                        queue.offer(i);
                 }
             }
         }
         
-
-        
-        // STEP 6:
-        // Finally, check the counter, if all the courses are processed rightly, then we expect the counter for all the courses to be zero.
-        for(int c : counter){
-            if(c > 0)
+        for(int c : courses){
+            if(c > 0){
                 return false;
+            }
         }
-    
-        
         return true;
-        
     }
 }
