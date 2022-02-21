0c5cac0a9fc9b5957a69d41d629e4d269ead1538
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-21 07:28:18 -0600

    Time: 3 ms (92.47%), Space: 52.6 MB (83.18%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,24 +1,35 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea = 0;
-        int i = 0;
-        int j = height.length - 1;
-        while(i < j){
-            int currArea = Math.min(height[i], height[j]) * (j - i);
-            if(height[i] < height[j]){
-                i++;
+        
+        //APPROACH 1: BRUTE FORCE -- TLE
+//         int maxValue = Integer.MIN_VALUE;
+//         for(int i = 0; i< height.length; i++){
+//             int value = 0;
+//             for(int j = i + 1; j < height.length; j++){
+//                 value = Math.min(height[i], height[j]) * (j - i);
+//                 maxValue = Math.max(value, maxValue);          
+//             }
+  
+//         }
+//         return maxValue;
+        
+        int start = 0;
+        int end = height.length - 1;
+        int maxValue = Integer.MIN_VALUE;
+        while(start < end){
+            
+            int value = Math.min(height[start], height[end]) * (end - start);
+            maxValue = Math.max(maxValue, value);
+            
+            if(height[start] < height[end]){
+                start++;
+            }else if(height[end] < height[start]){
+                end--;
+            } else{
+                start++;
+                end--;
             }
-            else if(height[i] > height[j]){
-                j--;
-            }
-            else{
-                i++;
-                j--;
-            }
-            maxArea = Math.max(currArea, maxArea);
         }
-     
-        return maxArea;
-    
+        return maxValue;
     }
 }
