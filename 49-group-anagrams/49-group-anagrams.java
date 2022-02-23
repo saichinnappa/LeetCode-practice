6457decea52ff0402320b0fc6255a479c1b4cc0d
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 07:47:34 -0600

    Time: 12 ms (67.23%), Space: 55.7 MB (33.38%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,21 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        if(strs.length == 0)
-            return result;
+        // List<List<String>> result = new ArrayList<>();
+        // if(strs.length == 0)
+        //     return result;
         Map<String, List<String>> map = new HashMap();
         for(String s : strs){
-            // String x = new String(s);
             char[] arr = s.toCharArray();
             Arrays.sort(arr);
             String sorted = new String(arr);
             map.putIfAbsent(sorted, new ArrayList());
             map.get(sorted).add(s);
         }
-        
-//         for(Map.Entry<String, List<String>> entry : map.entrySet()){
-//             result.add(entry.getValue());
-//         }
+    
         
         return new ArrayList(map.values());
     }
