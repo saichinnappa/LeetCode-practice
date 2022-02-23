e934411dc2ee93f8297d30b6bb4b12306fe1cea4
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 06:41:35 -0600

    Time: 0 ms (100.00%), Space: 42.8 MB (20.84%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,30 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int nrows = matrix.length;
-        int ncols = matrix[0].length;
-        boolean[][] swap = new boolean[nrows][ncols];
-        
-        for(int i = 0; i < nrows; i++){
-            for(int j = 0; j < ncols; j++){
-                if(i !=j && !swap[i][j]){
+        for(int i = 0; i< matrix.length; i++){
+            for(int j = i + 1; j < matrix[0].length; j++){
+                if(i != j){
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
-                    swap[i][j] = true;
-                    swap[j][i] = true;
                 }
             }
         }
         
-        for(int[] row : matrix){
-            int i = 0;
-            int j = row.length - 1;
-            while(i < j){
-                int temp = row[i];
-                row[i] = row[j];
-                row[j] = temp;
-                i++;
-                j--;
+        int length = matrix[0].length - 1;
+        for(int i = 0; i < matrix.length ; i++){
+            for(int j = 0; j < matrix[i].length / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length - j];
+                matrix[i][length - j] = temp;
             }
         }
     }
