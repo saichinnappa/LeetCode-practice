1c25068760d676dfdf0a0b1a72c3f3d77ab25a01
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 05:58:53 -0600

    Time: 187 ms (13.49%), Space: 197.7 MB (5.23%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,56 +10,43 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        //APPROACH 1: MERGE ONE BY ONE, AFTER FIRST TWO ARE MERGED
-//         if(lists.length == 0)
-//             return null;
-//         if(lists.length == 1)
-//             return lists[0];
+        if(lists.length == 0) return null;
+        if(lists.length == 1) return lists[0];
         
-//         ListNode head = mergeLists(lists[0], lists[1]);
-//         for(int i = 2; i < lists.length; i++){
-//             head = mergeLists(head, lists[i]);
-//         }
+        ListNode head = mergeTwoLists(lists[0], lists[1]);
+        ListNode result = head;
         
-//         return head;
-        
-        //APPROACH 2: Merge Sort
-        int interval = 1;
-        int length = lists.length;
-        while(interval < length){
-            for(int i = 0; i < length - interval; i += interval * 2){
-                lists[i] = mergeLists(lists[i], lists[i + interval]);
-            }
-            interval *= 2;
+        for(int i = 2; i < lists.length; i++){
+            head = mergeTwoLists(head, lists[i]);
         }
         
-        return lists.length > 0 ? lists[0] : null;
-      
+        return head;
     }
     
     
-    ListNode mergeLists(ListNode l1, ListNode l2){
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode temp = new ListNode(-1);
+        ListNode head = temp;
         while(l1 != null && l2 != null){
             if(l1.val < l2.val){
-                head.next = new ListNode(l1.val);
+                temp.next = new ListNode(l1.val);
                 l1 = l1.next;
             } else if (l2.val < l1.val){
-                head.next = new ListNode(l2.val);
+                temp.next = new ListNode(l2.val);
                 l2 = l2.next;
             } else{
-                head.next = new ListNode(l1.val);
-                head = head.next;
+                temp.next = new ListNode(l1.val);
                 l1 = l1.next;
-                head.next = new ListNode(l2.val);
+                temp = temp.next;
+                temp.next = new ListNode(l2.val);
                 l2 = l2.next;
             }
-            head = head.next;
+            temp = temp.next;
         }
-        head.next = (l1 == null) ? l2 : l1;
-        return dummy.next;
+        temp.next = (l1 == null) ? l2 : l1;
+        
+        return head.next;
     }
-
-    
 }
