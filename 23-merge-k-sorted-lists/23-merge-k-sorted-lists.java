231ab72a711f796c744750def835bd1135fe5797
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 06:17:09 -0600

    Time: 7 ms (57.68%), Space: 50.7 MB (5.23%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -25,13 +25,11 @@ class Solution {
         int interval = 1;
         int length = lists.length;
         while(interval < length){
-            for(int i = 0; i < length - interval; i += (interval * 2)){
+            for(int i = 0; i < length - interval; i += interval * 2){
                 lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
             }
-            
             interval *= 2;
         }
-        
         return lists[0];
         
     }
