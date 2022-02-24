f8a7d2d7d831aca8789db857a5710086d1ed118e
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 20:00:17 -0600

    Time: 182 ms (44.55%), Space: 42.1 MB (38.56%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,35 +1,44 @@
 class Solution {
-    boolean[][] visited;
     boolean exists = false;
+       boolean[][] visited;
     public boolean exist(char[][] board, String word) {
         visited = new boolean[board.length][board[0].length];
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    backtrack(i, j, 0, board, word);
+                    backtrack(board, word, 0, i, j);
+                    if(exists){
+                        System.out.println("---=here--");
+                        return true;
+                    }
                 }
             }
         }
+        
         return exists;
     }
     
-    public boolean backtrack(int i, int j, int index, char[][] board, String word){
-        if(i > -1 && i < board.length && j > -1 && j < board[0].length && index < word.length()
-          && !visited[i][j] && board[i][j] == word.charAt(index)){
-            visited[i][j] = true;
-        } else
-            return false;
+    
+    void backtrack(char[][] board, String word, int index, int row, int col){
+        int x = 0;
+        if(row > -1 && row < board.length && col > -1 && col < board[0].length && index < word.length() && board[row][col] == word.charAt(index) && !visited[row][col]){
+            visited[row][col] = true;
+        } else{
+            return;
+        }
         
-        if(index == word.length() - 1)
+
+        if(word.length() - 1 == index){
+                    System.out.println(index);
             exists = true;
+            return;
+        }
         
-        backtrack(i, j + 1, index + 1, board, word);
-        backtrack(i + 1, j, index + 1, board, word);
-        backtrack(i, j - 1, index + 1, board, word);
-        backtrack(i - 1, j, index + 1, board, word);
-        visited[i][j] = false;
-        return exists;
+        backtrack(board, word, index + 1, row + 1, col);
+        backtrack(board, word, index + 1, row, col + 1);
+        backtrack(board, word, index + 1, row - 1, col);
+        backtrack(board, word, index + 1, row, col - 1);
+        visited[row][col] = false;   
+        
     }
-    
-    
 }
