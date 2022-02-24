40323345abe612711166aa98746b7f182acfff9f
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-23 19:38:20 -0600

    Time: 8 ms (83.61%), Space: 42.5 MB (65.55%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,49 +1,56 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length())
-            return "";
-        Map<Character, Integer> map = new HashMap();
+        int tLength = t.length();
+        int sLength = s.length();
+        if(tLength > sLength) return "";
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
         char[] sarr = s.toCharArray();
         char[] tarr = t.toCharArray();
-        for(char c : tarr){
-            if(!map.containsKey(c)){
-                map.put(c, 0);
-            }
-            map.put(c,map.get(c)+1);
-        }
-        int match = 0;
-        int minLength = Integer.MAX_VALUE;
         int start = 0;
         int subStrStart = 0;
+        int minLength = 0;
+        
+        for(int i = 0; i < tarr.length; i++){
+            map.put(tarr[i], map.getOrDefault(tarr[i], 0) + 1);
+        }
+        
+        
+        int match = 0;
         for(int i = 0; i < sarr.length; i++){
-            char c = sarr[i];
-            if(map.containsKey(c)){
-                map.put(c, map.get(c) - 1);
-                if(map.get(c) >= 0){
+            if(map.containsKey(sarr[i])){
+                map.put(sarr[i], map.get(sarr[i]) - 1);
+                if(map.get(sarr[i]) == 0){
                     match++;
                 }
             }
-            
-            while(match == t.length()){
+            while(match == map.size()){
+                
+                if(minLength == 0)
+                    minLength = i - start + 1;
                 if(minLength > i - start + 1){
                     minLength = i - start + 1;
                     subStrStart = start;
                 }
-                char leftChar = sarr[start];
-                start++;
-                if(map.containsKey(leftChar)){
-                    if(map.get(leftChar) == 0){
+
+                // System.out.println(s.substring(subStrStart, subStrStart + minLength));
+                
+                if(map.containsKey(sarr[start])){
+                    map.put(sarr[start], map.get(sarr[start]) + 1);
+                    if(map.get(sarr[start]) > 0){
                         match--;
                     }
-                    map.put(leftChar, map.get(leftChar) + 1);
                 }
+                                start++;
                 
             }
             
+            
         }
         
-    
         
-        return minLength  > s.length() ? "" : s.substring(subStrStart, subStrStart+minLength);
+        return s.substring(subStrStart, subStrStart + minLength);
+        
     }
 }
