04f59ec4c6ce98499a425ea136db832e6ea6b98f
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-24 18:16:17 -0600

    Time: 2 ms (76.72%), Space: 40 MB (45.69%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,37 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] arr = s.toCharArray();
-        if(s.length() == 1)
+        if(s.length() < 2)
             return false;
-        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> map = new HashMap();
         map.put('}', '{');
-        map.put(')', '(');
         map.put(']', '[');
+        map.put(')', '(');
         
-        if(map.containsKey(s.charAt(0))){
-            return false;
-        }
-        
-        Stack<Character> stack = new Stack();
+        char[] input = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
         
         
-        for(int i = 0; i < arr.length; i++){
-            if(!map.containsKey(arr[i]))
-                stack.push(arr[i]);
-            else{
-
-                if(stack.size() == 0) return false;
-                char top = stack.pop();
-                if(map.get(arr[i]) != top)
-                    return false;
-                // }    
+        for(int i = 0; i < input.length; i++){
+            if(!map.containsKey(input[i])){
+                stack.push(input[i]);
             }
-            
+            else{
+                if(!stack.isEmpty()){
+                    char top = stack.pop();
+                    if(map.get(input[i]) != top){
+                        return false;
+                    }    
+                } else{
+                    return false;
+                }  
+            }
         }
-
         
         return stack.size() == 0;
-        
-        
     }
 }
