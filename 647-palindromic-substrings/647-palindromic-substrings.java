671b04ada62d2bc6db261defa2a886c0e180a4ac
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-24 19:11:47 -0600

    Time: 12 ms (43.96%), Space: 44.5 MB (19.61%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,34 +1,34 @@
 class Solution {
     public int countSubstrings(String s) {
-        if(s.length() <= 1)
-            return s.length();
+        int length = s.length();
+        if(length < 2)
+            return length;
         int result = 0;
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
+        boolean[][] dp = new boolean[length][length];
         
-        for(int i = 0; i < s.length(); i++){
+        char[] arr = s.toCharArray();
+        
+        for(int i = 0; i < length; i++){
             dp[i][i] = true;
             result++;
         }
         
-        for(int i = 0; i < s.length() - 1; i++){
-            if(s.charAt(i) == s.charAt(i+1)){
+        for(int i = 0; i < length - 1; i++){
+            if(arr[i] == arr[ i+ 1]){
                 dp[i][i+1] = true;
                 result++;
             }
         }
         
-        for(int k = 3; k <= n; k++){
-            for(int i = 0; i < n - k + 1; i++){
+        for(int k = 3; k <= length; k++){
+            for(int i = 0; i <= length - k; i++){
                 int j = i + k - 1;
-
-                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                if(arr[i] == arr[j] && dp[i+1][j - 1]){
                     dp[i][j] = true;
                     result++;
                 }
             }
         }
-        
         return result;
     }
 }
