39d0786c7bc7d38e12442d3ac5489b74fdb5e798
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-02-27 12:00:18 -0600

    Time: 3 ms (97.37%), Space: 41.8 MB (86.76%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -13,33 +13,20 @@
  *     }
  * }
  */
-// class Solution {
-//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-//         if(root == null)
-//             return false;
-        
-//         return isSameTree(root, subRoot) || isSameTree(root.left, subRoot) || isSameTree(root.right, subRoot);
-//     }
-    
-//     boolean isSameTree(TreeNode root, TreeNode subRoot){
-//         if(root == null && subRoot == null)
-//             return true;
-//         if(root == null || subRoot == null)
-//             return false;
-//         return (root.val == subRoot.val) && isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
-//     }
-    
-// }
-
-
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null) return false;
-        return isSame(root,subRoot) || isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        if(root == null)
+            return false;
+        
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    public boolean isSame(TreeNode r1, TreeNode r2){
-        if(r1==null && r2==null) return true;
-        if(r1==null || r2==null) return false;
-        return r1.val==r2.val && isSame(r1.left,r2.left) && isSame(r1.right,r2.right);
+    
+    boolean isSameTree(TreeNode root, TreeNode subRoot){
+        if(root == null && subRoot == null)
+            return true;
+        if(root == null || subRoot == null)
+            return false;
+        return (root.val == subRoot.val) && isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
     }
+    
 }
