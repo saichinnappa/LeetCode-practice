c05d847fe6f6038e49d24981c17e3feb41c320a9
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-03-09 19:30:59 -0600

    Time: 1 ms (98.28%), Space: 40.3 MB (59.61%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.java
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -1,23 +1,69 @@
+// class HitCounter {
+
+//     Queue<Integer> queue;
+//     public HitCounter() {
+//         queue = new LinkedList<>();
+//     }
+    
+//     public void hit(int timestamp) {
+//         queue.offer(timestamp);
+//     }
+    
+//     public int getHits(int timestamp) {
+//         while(!queue.isEmpty()){
+//             int diff = timestamp - queue.peek();
+//             if(diff >= 300) queue.poll();
+//             else break;
+//         }
+//         return queue.size();
+//     }
+// }
+
 class HitCounter {
 
-    Queue<Integer> queue;
+    Deque<Pair> queue;
+    int total = 0;
     public HitCounter() {
         queue = new LinkedList<>();
     }
     
     public void hit(int timestamp) {
-        queue.offer(timestamp);
+        if(!queue.isEmpty()){
+            Pair first = queue.pollLast();
+            if(first.timestamp == timestamp){
+                queue.offer(new Pair(timestamp, first.count + 1));
+            } else{
+                queue.offer(first);
+                queue.offer(new Pair(timestamp, 1));
+            }
+        } else{
+            queue.offer(new Pair(timestamp, 1));
+        }
+        total++;
     }
     
     public int getHits(int timestamp) {
-        while(!queue.isEmpty()){
-            int diff = timestamp - queue.peek();
-            if(diff >= 300) queue.poll();
-            else break;
-        }
-        return queue.size();
+        // System.out.println(queue.getFirst().timestamp);
+       while(!queue.isEmpty() && timestamp - queue.peek().timestamp >= 300){
+           total -= queue.poll().count;
+       }
+        
+        
+        return total;
     }
 }
+
+class Pair{
+    int timestamp;
+    int count;
+    Pair(int timestamp, int count){
+        this.timestamp = timestamp;
+        this.count = count;
+    }
+}
+
+
+
 
 /**
  * Your HitCounter object will be instantiated and called as such:
