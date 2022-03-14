4e69e5610e39a40056920344860db7c654c98a38
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-03-14 14:56:09 -0500

    Time: 208 ms, Memory: 79.9 MB - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length() == 1)
+            return s;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        char[] arr = s.toCharArray();
+        int length = 0;
+        int start = 0;
+        
+        for(int i = 0; i < n; i++){
+            dp[i][i] = true;
+            length = 1;
+            start = i;
+        }
+        
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i] == arr[i + 1]){
+                dp[i][i + 1] = true;
+                length = 2;
+                start = i;
+            }
+        }
+        
+        
+        for(int i = 3; i <= n ; i++){
+            for(int j = 0; j <= n - i; j++){
+                int k = j + i - 1;
+                if(arr[j] == arr[k] && dp[j + 1][k - 1]){
+                    dp[j][k] = true;
+                    length = i;
+                    start = j;
+                }
+            }
+        }
+        return s.substring(start, start + length);
+    }
+}
