92a69bffa1ddeaed6c0e68fb07ab5d961e3fc012
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-03-26 07:10:16 -0500

    Time: 246 ms (29.85%), Space: 117.4 MB (15.99%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,33 +1,35 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>(wordList);
-        Queue<String> queue = new ArrayDeque();
+        Queue<String> queue = new LinkedList<>();
+        int dist = 1;
         queue.offer(beginWord);
-        int count = 1;
         while(!queue.isEmpty()){
             int size = queue.size();
+            int count = 1;
             while(size != 0){
+                size--;
                 String first = queue.poll();
-                char[] arr =  first.toCharArray();
-                for(int i = 0; i < arr.length; i++){
-                    char temp = arr[i];
+                for(int i = 0; i < first.length(); i++){
+                    String newString = first;
                     for(char c = 'a'; c <= 'z'; c++){
-                        arr[i] = c;
-                        String next = new String(arr);
-                        if(set.contains(next)){
-                            if(next.equals(endWord))
-                                return count+1;
-                            queue.offer(next);
-                            set.remove(next);
+                       StringBuilder sb = new StringBuilder(newString);
+                        sb.setCharAt(i, c);
+                        if(set.contains(sb.toString())){
+                             // System.out.println("FOUND-->"+ sb.toString());
+                            if(sb.toString().equals(endWord)){
+                               return dist + 1;
+                            }
+                            queue.offer(sb.toString());
+                            set.remove(sb.toString());
+                            
                         }
                     }
-                    arr[i] = temp;
                 }
-                size--;
             }
-            count++;
+            dist++;
+            
         }
         return 0;
     }
-    
 }
