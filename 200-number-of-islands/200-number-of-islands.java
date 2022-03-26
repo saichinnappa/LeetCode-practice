9f46a9827ddb39401ed00bc21c44fad64c96dc01
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-03-26 08:07:18 -0500

    Time: 9 ms (23.38%), Space: 49.6 MB (98.41%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,17 +2,44 @@ class Solution {
     public int numIslands(char[][] grid) {
         //DFS
         int result = 0;
+        Queue<Pair> queue = new LinkedList<>();
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
                 if(grid[i][j] == '1'){
                     result++;
-                    dfs(grid, i , j);
+                    queue.offer(new Pair(i, j));
+                                            grid[i][j] = 'X';
+                    while(!queue.isEmpty()){
+                        Pair p = queue.poll();
+
+                        List<Pair> neigh = getNeighbors(p.row, p.col, grid);
+                        for(Pair p1 : neigh){
+                            queue.offer(p1);
+                        }
+                    }
+                    
+                    // dfs(grid, i , j);
                 }
             }
         }
         return result;
     }
     
+    
+    
+    List<Pair> getNeighbors(int row, int col, char[][] grid){
+        List<Pair> list = new ArrayList<>();
+        int[][] dist = {{1, 0}, {0,1}, {-1, 0}, {0, -1}};
+        for(int i = 0; i < dist.length; i++){
+            int newRow = row + dist[i][0];
+            int newCol = col + dist[i][1];
+            if(newRow < grid.length && newRow > -1 && newCol < grid[0].length && newCol > -1 && grid[newRow][newCol] == '1'){
+                                     grid[newRow][newCol] = 'X';
+                list.add(new Pair(newRow, newCol));
+            } 
+        }
+        return list;
+    }
     
     void dfs(char[][] grid, int i, int j){
         if(i < grid.length && i > -1 && j < grid[0].length && j > -1 && grid[i][j] == '1'){
@@ -26,4 +53,13 @@ class Solution {
         dfs(grid, i, j - 1);
     }
     
+}
+
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
 }
