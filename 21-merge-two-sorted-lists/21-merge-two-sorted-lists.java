70be80a6d415fc86c03dff0d417f4db21dcadc1e
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-03 09:38:09 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (98.65%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,29 +10,42 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-        ListNode dummy = new ListNode(-101);
-        ListNode result = dummy;
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                dummy.next = new ListNode(list1.val);
-                list1 = list1.next;
-            } else if(list1.val > list2.val){
-                dummy.next = new ListNode(list2.val);
-                list2 = list2.next;
-            } else{
-                dummy.next = new ListNode(list1.val);
-                list1 = list1.next;
-                dummy = dummy.next;
-                dummy.next = new ListNode(list2.val);
-                list2 = list2.next;
-            }
-            dummy = dummy.next;
-        }
+//         if(list1 == null) return list2;
+//         if(list2 == null) return list1;
+//         ListNode dummy = new ListNode(-101);
+//         ListNode result = dummy;
+//         while(list1 != null && list2 != null){
+//             if(list1.val < list2.val){
+//                 dummy.next = new ListNode(list1.val);
+//                 list1 = list1.next;
+//             } else if(list1.val > list2.val){
+//                 dummy.next = new ListNode(list2.val);
+//                 list2 = list2.next;
+//             } else{
+//                 dummy.next = new ListNode(list1.val);
+//                 list1 = list1.next;
+//                 dummy = dummy.next;
+//                 dummy.next = new ListNode(list2.val);
+//                 list2 = list2.next;
+//             }
+//             dummy = dummy.next;
+//         }
         
-        dummy.next = (list1 == null) ? list2 : list1;
-        return result.next;
+//         dummy.next = (list1 == null) ? list2 : list1;
+//         return result.next;
+        // private ListNode mergeSortedLists(ListNode list1, ListNode list2) {
+		if (list1 == null)
+			return list2;
+		if (list2 == null)
+			return list1;
+
+		if (list1.val < list2.val) {
+			list1.next = mergeTwoLists(list1.next, list2);
+			return list1;
+		} else {
+			list2.next = mergeTwoLists(list2.next, list1);
+			return list2;
+		}
     }
     
 
