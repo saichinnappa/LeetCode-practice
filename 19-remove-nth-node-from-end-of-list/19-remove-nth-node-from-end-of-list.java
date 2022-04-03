099e396a1b664e13ae6c7ac1c4e7077c1cf9f298
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-03 08:06:44 -0500

    Time: 1 ms (63.23%), Space: 42.4 MB (46.33%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -10,23 +10,44 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int totalNodes = 0;
-        ListNode curr = head;
-        while(curr != null){
-            curr = curr.next;
-            totalNodes++;
-        }
-        ListNode prev = new ListNode(-1);
-        ListNode result = prev;
-        int a = totalNodes - n;
+//         int totalNodes = 0;
+//         ListNode curr = head;
+//         while(curr != null){
+//             curr = curr.next;
+//             totalNodes++;
+//         }
+//         ListNode prev = new ListNode(-1);
+//         ListNode result = prev;
+//         int a = totalNodes - n;
         
-        while(a != 0){
-            prev.next = head;
-            head = head.next;
-            prev = prev.next;
-            a--;
+//         while(a != 0){
+//             prev.next = head;
+//             head = head.next;
+//             prev = prev.next;
+//             a--;
+//         }
+//         prev.next = head.next;
+//         return result.next;
+        
+        
+        ListNode slow = new ListNode(-1);
+        ListNode fast = new ListNode(-1);
+        ListNode result = slow;
+        slow.next = head;
+        fast.next = head;
+        
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
         }
-        prev.next = head.next;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
         return result.next;
+        
+        
     }
 }
