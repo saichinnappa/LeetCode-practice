e6f4dc5a43c1a4571d48f224d8561a74762bdec7
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-04 10:07:08 -0500

    Time: 0 ms (100.00%), Space: 42.7 MB (64.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,15 +1,15 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        boolean[][] track = new boolean[matrix.length][matrix[0].length];
+        // boolean[][] track = new boolean[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(i != j && !track[i][j]){
+            for(int j = i + 1; j < matrix[0].length; j++){
+                // if(i != j && !track[i][j]){
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
-                    track[i][j] = true;
-                    track[j][i] = true;
-                }
+                    // track[i][j] = true;
+                    // track[j][i] = true;
+                // }
             }
         }
         int n = matrix[0].length - 1;
