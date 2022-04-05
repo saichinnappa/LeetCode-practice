1ed65aabc7f3e0ef85638bdd9aa26e0eb1e5cd50
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-05 07:41:56 -0500

    Time: 0 ms (100.00%), Space: 42.5 MB (20.84%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,58 +1,51 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        
         int r = matrix.length;
         int c = matrix[0].length;
         
+        List<Integer> result = new ArrayList<>();
         boolean[][] visited = new boolean[r][c];
         
         int up = 0;
-        int right = c - 1;
+        int left= 0;
+        int right = c -1;
         int down = r - 1;
-        int left = 0;
         
         while(result.size() != r * c){
             
-            //L -> R
-            for(int i = left; i <= right; i++){
+            for(int i = left; i <=right; i++){
                 if(!visited[up][i]){
                     visited[up][i] = true;
                     result.add(matrix[up][i]);
                 }
             }
             
+            up++;
             
-            
-            for(int i = up + 1; i <= down; i++){
+            for(int i = up; i <= down; i++){
                 if(!visited[i][right]){
                     visited[i][right] = true;
                     result.add(matrix[i][right]);
                 }
             }
+            right--;
             
-            
-            
-            for(int i = right - 1; i >= left; i--){
+            for(int i = right; i >= left; i--){
                 if(!visited[down][i]){
                     visited[down][i] = true;
                     result.add(matrix[down][i]);
                 }
             }
             
+            down--;
             
-            
-            for(int i = down ; i >= up; i--){  
+            for(int i = down; i >= up; i--){
                 if(!visited[i][left]){
                     visited[i][left] = true;
                     result.add(matrix[i][left]);
                 }
             }
-            up++;
             left++;
-            right--;
-            down--;
-        
         }
         return result;
     }
