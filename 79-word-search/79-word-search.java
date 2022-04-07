85df3fa5d47d764c279f6214e36eceee6b5f58af
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-06 21:47:10 -0500

    Time: 151 ms (54.51%), Space: 41.9 MB (71.41%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,42 +1,43 @@
 class Solution {
     boolean exists = false;
-       boolean[][] visited;
+    boolean[][] visited;
+    char[][] board;
+    int result;
     public boolean exist(char[][] board, String word) {
+        this.board = board;
         visited = new boolean[board.length][board[0].length];
-        for(int i = 0; i < board.length; i++){
+        for(int i =0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    backtrack(board, word, 0, i, j);
-                    if(exists){
+                    dfs(i, j, 0, word);
+                    if(exists)
                         return true;
-                    }
                 }
             }
         }
-        
-        return exists;
+        return false;
     }
     
     
-    void backtrack(char[][] board, String word, int index, int row, int col){
-        int x = 0;
-        if(row > -1 && row < board.length && col > -1 && col < board[0].length && index < word.length() && board[row][col] == word.charAt(index) && !visited[row][col]){
+    void dfs(int row, int col, int index, String word){
+        if(row > -1 && row < board.length && col > -1 && col < board[0].length && index < word.length() && !visited[row][col] && word.charAt(index) == board[row][col]){
             visited[row][col] = true;
         } else{
             return;
         }
-        
-
-        if(word.length() - 1 == index){
-            exists = true;
-            return;
+        // if(word.charAt(index) == board[row][col]){
+            
+            if(index == word.length() - 1){
+                exists = true;
+                return;
+            // }
         }
         
-        backtrack(board, word, index + 1, row + 1, col);
-        backtrack(board, word, index + 1, row, col + 1);
-        backtrack(board, word, index + 1, row - 1, col);
-        backtrack(board, word, index + 1, row, col - 1);
-        visited[row][col] = false;   
-        
+        dfs(row + 1, col, index + 1, word);
+        dfs(row - 1, col, index + 1, word);
+        dfs(row, col + 1, index + 1, word);
+        dfs(row, col - 1, index + 1, word);
+        visited[row][col] = false;
     }
+    
 }
