f1d145312dd064e98896f7eb2a9ff8e1cddd99d2
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-07 14:17:36 -0500

    Time: 30 ms (70.56%), Space: 61.4 MB (53.45%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,51 +1,51 @@
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        // if(nums.length < 2)
-        //     return nums.length;
-        Set<Integer> set = new HashSet<Integer>();
-        int maxStreak = 0;
-        for(int num : nums)
-            set.add(num);
-        
-        for(int i = 0; i < nums.length; i++){
-            int current = nums[i];
-            if(!set.contains(current - 1)){
-                int count = 1;
-                while(set.contains(current + 1)){
-                    current += 1;
-                    count += 1;
-                }
-                maxStreak = Math.max(maxStreak, count);
-            }
-        }
-        
-        return maxStreak;
-    }
-}
-
 // class Solution {
 //     public int longestConsecutive(int[] nums) {
-//         Set<Integer> num_set = new HashSet<Integer>();
-//         for (int num : nums) {
-//             num_set.add(num);
-//         }
-
-//         int longestStreak = 0;
-
-//         for (int num : num_set) {
-//             if (!num_set.contains(num-1)) {
-//                 int currentNum = num;
-//                 int currentStreak = 1;
-
-//                 while (num_set.contains(currentNum+1)) {
-//                     currentNum += 1;
-//                     currentStreak += 1;
+//         // if(nums.length < 2)
+//         //     return nums.length;
+//         Set<Integer> set = new HashSet<Integer>();
+//         int maxStreak = 0;
+//         for(int num : nums)
+//             set.add(num);
+        
+//         for(int i = 0; i < nums.length; i++){
+//             int current = nums[i];
+//             if(!set.contains(current - 1)){
+//                 int count = 1;
+//                 while(set.contains(current + 1)){
+//                     current += 1;
+//                     count += 1;
 //                 }
-
-//                 longestStreak = Math.max(longestStreak, currentStreak);
+//                 maxStreak = Math.max(maxStreak, count);
 //             }
 //         }
-
-//         return longestStreak;
+        
+//         return maxStreak;
 //     }
 // }
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
