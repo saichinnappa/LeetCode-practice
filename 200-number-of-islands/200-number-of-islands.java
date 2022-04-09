2596bc63840d93fc67126094c5597eaea2e16443
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-09 17:40:11 -0500

    Time: 5 ms (48.13%), Space: 57.4 MB (48.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,65 +1,32 @@
 class Solution {
+    int result = 0;
+    int row, col;
+    char[][] grid;
     public int numIslands(char[][] grid) {
-        //DFS
-        int result = 0;
-        Queue<Pair> queue = new LinkedList<>();
+        this.grid = grid;
+        this.row = grid.length;
+        this.col = grid[0].length;
         for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
                     result++;
-                    queue.offer(new Pair(i, j));
-                                            grid[i][j] = 'X';
-                    while(!queue.isEmpty()){
-                        Pair p = queue.poll();
-
-                        List<Pair> neigh = getNeighbors(p.row, p.col, grid);
-                        for(Pair p1 : neigh){
-                            queue.offer(p1);
-                        }
-                    }
+                    dfs(i, j);
                     
-                    // dfs(grid, i , j);
                 }
             }
         }
+        
         return result;
     }
     
-    
-    
-    List<Pair> getNeighbors(int row, int col, char[][] grid){
-        List<Pair> list = new ArrayList<>();
-        int[][] dist = {{1, 0}, {0,1}, {-1, 0}, {0, -1}};
-        for(int i = 0; i < dist.length; i++){
-            int newRow = row + dist[i][0];
-            int newCol = col + dist[i][1];
-            if(newRow < grid.length && newRow > -1 && newCol < grid[0].length && newCol > -1 && grid[newRow][newCol] == '1'){
-                                     grid[newRow][newCol] = 'X';
-                list.add(new Pair(newRow, newCol));
-            } 
-        }
-        return list;
-    }
-    
-    void dfs(char[][] grid, int i, int j){
-        if(i < grid.length && i > -1 && j < grid[0].length && j > -1 && grid[i][j] == '1'){
-            grid[i][j] = 'X';
-        } else{
+    void dfs(int r, int c){
+        if(r < 0 || r >= row || c < 0 || c >= col || this.grid[r][c] != '1')
             return;
-        }
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1,  j);
-        dfs(grid, i, j - 1);
-    }
-    
-}
-
-class Pair{
-    int row;
-    int col;
-    Pair(int row, int col){
-        this.row = row;
-        this.col = col;
+        this.grid[r][c] = 'X';
+        
+        dfs(r + 1, c);
+        dfs(r, c + 1);
+        dfs(r - 1, c);
+        dfs(r, c - 1);
     }
 }
