a3e086ee2c934ee52573c081d7998401ef18504d
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-25 14:13:22 -0500

    Time: 52 ms, Memory: 47.4 MB - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
-        
+        List<List<Integer>> result = new ArrayList<>();
         if(nums.length == 0)
             return result;
-        
         Arrays.sort(nums);
-        Set<Integer> set = new HashSet();
-        
-        // int j = 0, k = nums.length - 1;
-        
-        for(int i = 0; i< nums.length; i++){
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
             if(!set.contains(nums[i])){
-                set.add(nums[i]);
-                List<Integer> list= new ArrayList();
-                int j = i + 1, k = nums.length - 1;
+                int j = i + 1;
+                int k = nums.length - 1;
+                
                 while(j < k){
+                    List<Integer> list = new ArrayList<>();
                     int sum = nums[i] + nums[j] + nums[k];
                     if(sum == 0){
-                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        list.addAll(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while(j < n - 1 && nums[j] == nums[j + 1]){
+                            j++;
+                        }
                         j++;
                         k--;
-                        while(j < k && nums[j] == nums[j - 1]){
-                            j++;
-                        } 
-
-                    } else if (sum > 0){
+                    } else if(sum > 0){
                         k--;
                     } else{
                         j++;
                     }
+                if(list.size() > 0)
+                    result.add(list);
                 }
+                set.add(nums[i]);
+
             }
         }
         return result;
