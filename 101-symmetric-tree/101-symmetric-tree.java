253d60b3ccc7d2e44f865d7605e0f5ef3a0db50e
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-04-27 11:00:20 -0500

    Time: 1 ms (51.88%), Space: 42.7 MB (21.43%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,42 +15,21 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        //recursive
-        //return helper(root.left, root.right);
-        //iterative
-        if(root == null)
-            return true;
-        if((root.left != null && root.right == null) || (root.left == null && root.right != null))
+        if(root == null)   
             return false;
-        Deque<TreeNode> queue = new LinkedList<>();
-        queue.offer(root.left);
-        queue.offer(root.right);
-        while(!queue.isEmpty()){
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            if(t1 == null && t2 == null) continue;
-            if((t1 == null && t2 != null) || (t1 != null && t2 == null)) return false;
-            if(t1.val != t2.val) return false;
-            queue.offer(t1.left);
-            queue.offer(t2.right);
-            queue.offer(t1.right);
-            queue.offer(t2.left);
-        
-        }
-        
-        return true;
+        return helper(root.left, root.right);
     }
     
     boolean helper(TreeNode t1, TreeNode t2){
         if(t1 == null && t2 == null)
             return true;
-        else if(t1 == null && t2 != null)
+        if(t1 == null && t2 != null)
             return false;
-        else if(t1 != null && t2 == null)
+        if(t1 != null && t2 == null)
             return false;
-        else if(t1.val != t2.val)
+        if(t1.val != t2.val)
             return false;
-        else
-            return helper(t1.left, t2.right) && helper(t1.right, t2.left);
+            
+        return helper(t1.left, t2.right) && helper(t1.right,  t2.left);
     }
 }
