e29db36850ac43365035e3f22ae9826704c8297b
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-05-07 21:21:32 -0500

    Time: 117 ms (72.59%), Space: 103.4 MB (55.09%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -1,81 +1,30 @@
 class FileSystem {
+    
+    Map<String, Integer> map;
 
-    // The TrieNode data structure.
-    class TrieNode {
-        
-        String name;
-        int val = -1;
-        Map<String, TrieNode> map = new HashMap<>();
-        
-        TrieNode (String name) {
-            this.name = name;
-        }
-    }
-    
-    TrieNode root;
-    
-    // Root node contains the empty string.
     public FileSystem() {
-        this.root = new TrieNode("");
+        map = new HashMap<>();
     }
     
     public boolean createPath(String path, int value) {
         
-        // Obtain all the components
-        String[] components = path.split("/");
-        
-        // Start "curr" from the root node.
-        TrieNode cur = root;
-        
-        // Iterate over all the components.
-        for (int i = 1; i < components.length; i++) {
-            
-            String currentComponent = components[i];
-            
-            // For each component, we check if it exists in the current node's dictionary.
-            if (cur.map.containsKey(currentComponent) == false) {
-                
-                // If it doesn't and it is the last node, add it to the Trie.
-                if (i == components.length - 1) {
-                    cur.map.put(currentComponent, new TrieNode(currentComponent));
-                } else {
-                    return false;
-                }    
-            }
-            
-            cur = cur.map.get(currentComponent);
-        }
-        
-        // Value not equal to -1 means the path already exists in the trie. 
-        if (cur.val != -1) {
+        if(path == "" || path == "/" || map.containsKey(path))
             return false;
+        int lastIndex = path.lastIndexOf("/");
+        if(lastIndex == 0){
+                map.put(path, value);
+                return true;
+        } else{
+            String subStr = path.substring(0, lastIndex);
+            if(map.containsKey(subStr)){
+                map.put(path, value);
+                return true;
+            }
         }
-        
-        cur.val = value;
-        return true;
+        return false;
     }
     
     public int get(String path) {
-        
-        // Obtain all the components
-        String[] components = path.split("/");
-        
-        // Start "curr" from the root node.
-        TrieNode cur = root;
-        
-        // Iterate over all the components.
-        for (int i = 1; i < components.length; i++) {
-            
-            String currentComponent = components[i];
-            
-            // For each component, we check if it exists in the current node's dictionary.
-            if (cur.map.containsKey(currentComponent) == false) {
-                return -1;   
-            }
-            
-            cur = cur.map.get(currentComponent);
-        }
-        
-        return cur.val;
+        return map.containsKey(path) ? map.get(path) : -1;
     }
 }
