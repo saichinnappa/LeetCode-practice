d8bf9a151892c19c876b73e51e94e114e2dcb6e7
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-05-16 08:40:48 -0500

    Time: 6 ms (21.90%), Space: 43.5 MB (16.08%) - LeetHub

--- 859-buddy-strings/859-buddy-strings.java
+++ 859-buddy-strings/859-buddy-strings.java
@@ -7,7 +7,10 @@ class Solution {
             return s.size() < A.length();
         }
         List<Integer> dif = new ArrayList<>();
-        for (int i = 0; i < A.length(); ++i) if (A.charAt(i) != B.charAt(i)) dif.add(i);
+        for (int i = 0; i < A.length(); ++i){
+            if (A.charAt(i) != B.charAt(i)) 
+                dif.add(i);
+        }
         return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
     
     }
