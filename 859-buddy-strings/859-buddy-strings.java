32632149ccb93aa1808b5c049bf4d84768ce3066
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-05-31 18:39:46 -0500

    Time: 8 ms (11.94%), Space: 43.5 MB (14.23%) - LeetHub

--- 859-buddy-strings/859-buddy-strings.java
+++ 859-buddy-strings/859-buddy-strings.java
@@ -1,17 +1,27 @@
 class Solution {
-    public boolean buddyStrings(String A, String B) {
-        if (A.length() != B.length()) return false;
-        if (A.equals(B)) {
-            Set<Character> s = new HashSet<Character>();
-            for (char c : A.toCharArray()) s.add(c);
-            return s.size() < A.length();
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length())
+            return false;
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> gmap = new HashMap<>();
+        if(s.equals(goal)){
+            for(int i = 0; i < s.length(); i++){
+                smap.put(s.charAt(i), i);
+            }
+            return smap.size() < s.length();
+                
         }
+        for(int j = 0; j < goal.length(); j++){
+            gmap.put(goal.charAt(j), j);
+        }
+        
         List<Integer> dif = new ArrayList<>();
-        for (int i = 0; i < A.length(); ++i){
-            if (A.charAt(i) != B.charAt(i)) 
+        for (int i = 0; i < s.length(); ++i){
+            if (s.charAt(i) != goal.charAt(i)) 
                 dif.add(i);
         }
-        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+        return dif.size() == 2 && s.charAt(dif.get(0)) == goal.charAt(dif.get(1)) && s.charAt(dif.get(1)) == goal.charAt(dif.get(0));
     
+        // return false;
     }
 }
