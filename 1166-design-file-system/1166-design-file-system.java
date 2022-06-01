8c3370b9b492b0f899ce7edbcac29f5b8d7eb2a9
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-01 08:58:35 -0500

    Time: 180 ms (28.91%), Space: 107.9 MB (49.64%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -1,26 +1,24 @@
 class FileSystem {
-    
-    Map<String, Integer> map;
 
+    Map<String, Integer> map = new HashMap<>();
+    
     public FileSystem() {
-        map = new HashMap<>();
+        
     }
     
     public boolean createPath(String path, int value) {
-        
-        if(path == "" || path == "/" || map.containsKey(path))
+        if(map.containsKey(path))
             return false;
         int lastIndex = path.lastIndexOf("/");
         if(lastIndex == 0){
-                map.put(path, value);
-                return true;
-        } else{
-            String subStr = path.substring(0, lastIndex);
-            if(map.containsKey(subStr)){
-                map.put(path, value);
-                return true;
-            }
+            map.put(path, value);
+            return true;
         }
+        String parentPath = path.substring(0, lastIndex);
+        if(map.containsKey(parentPath)){
+            map.put(path, value);
+            return true;
+        } 
         return false;
     }
     
@@ -28,3 +26,10 @@ class FileSystem {
         return map.containsKey(path) ? map.get(path) : -1;
     }
 }
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
