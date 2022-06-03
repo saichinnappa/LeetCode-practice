37c647c3a60b57b3e040fbeff2046785f2d65745
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-03 06:39:17 -0500

    Time: 6 ms (92.40%), Space: 44.3 MB (54.04%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,34 @@
+class Solution {
+    int i;
+
+    public int calculate(String s) {
+
+        int operand = 0;
+        int result = 0; // For the on-going result
+        int nextOperandSign = 1;  // 1 means positive, -1 means negative
+
+        while(i < s.length()) {
+
+            char ch = s.charAt(i++);
+
+            if (ch == ' ' || Character.isDigit(ch)) {
+                operand = (ch == ' ') ? operand : 10 * operand + (ch - '0');
+                
+            } else if (ch == '(') {
+                operand = calculate(s);
+                
+            } else if (ch == ')') {
+                break; // Sub-expression we were evaluating has ended. Exit now...
+                
+            } else { 
+                // If we're here, we just read the operator for the next operand/expression.
+                // Evaluate the existing expression already read, reset operand, set sign for next incoming operand.
+                result += nextOperandSign * operand;
+                nextOperandSign = ch == '+' ? 1 : -1;
+                operand = 0;
+            }
+        }
+
+        return result + (nextOperandSign * operand);
+    }
+}
