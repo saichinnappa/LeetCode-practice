af6c34e2518d2957ac6a57a6d093af2184526ae9
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-12 17:01:17 -0500

    Time: 6 ms (56.53%), Space: 47.8 MB (34.27%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,55 +1,103 @@
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//         Map<Integer, Set<Character>> rowSet = new HashMap<>();
+//         Map<Integer, Set<Character>> colSet = new HashMap<>();
+//         Map<Integer, Set<Character>> blockSet = new HashMap<>();
+        
+        
+//         int m = board.length;
+//         int n = board[0].length;
+        
+//         for(int i = 0; i < m; i++){
+//              rowSet.put(i, new HashSet<Character>());
+//             for(int j = 0; j < n; j++){
+//                 char val = board[i][j];
+//                 if(val != '.'){
+//                     if(!rowSet.get(i).contains(val)){
+//                         rowSet.get(i).add(val);
+//                     } else
+//                         return false;    
+//                 }
+//             }
+//         }
+        
+        
+//           for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 char val = board[i][j];
+//                 colSet.putIfAbsent(j, new HashSet<Character>());
+//                 if(val != '.'){
+//                     if(!colSet.get(j).contains(val)){
+//                         colSet.get(j).add(val);
+//                     } else
+//                         return false;    
+//                 }
+//             }
+//         }
+        
+//            for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 char val = board[i][j];
+//                 int blockValue = (i / 3) * 3 + (j / 3);
+//                 if(!blockSet.containsKey(blockValue))
+//                     blockSet.put(blockValue, new HashSet<Character>());
+//                 if(val != '.'){
+//                     if(!blockSet.get(blockValue).contains(val)){
+//                         blockSet.get(blockValue).add(val);
+//                     } else
+//                         return false;    
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Map<Integer, Set<Character>> rowSet = new HashMap<>();
-        Map<Integer, Set<Character>> colSet = new HashMap<>();
-        Map<Integer, Set<Character>> blockSet = new HashMap<>();
-        
-        
-        int m = board.length;
-        int n = board[0].length;
-        
-        for(int i = 0; i < m; i++){
-             rowSet.put(i, new HashSet<Character>());
-            for(int j = 0; j < n; j++){
-                char val = board[i][j];
-                if(val != '.'){
-                    if(!rowSet.get(i).contains(val)){
-                        rowSet.get(i).add(val);
-                    } else
-                        return false;    
+        int N = 9;
+
+        // Use hash set to record the status
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        for (int r = 0; r < N; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                // Check if the position is filled with number
+                if (val == '.') {
+                    continue;
                 }
+
+                // Check the row
+                if (rows[r].contains(val)) {
+                    return false;
+                }
+                rows[r].add(val);
+
+                // Check the column
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
             }
         }
-        
-        
-          for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                char val = board[i][j];
-                colSet.putIfAbsent(j, new HashSet<Character>());
-                if(val != '.'){
-                    if(!colSet.get(j).contains(val)){
-                        colSet.get(j).add(val);
-                    } else
-                        return false;    
-                }
-            }
-        }
-        
-           for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                char val = board[i][j];
-                int blockValue = (i / 3) * 3 + (j / 3);
-                if(!blockSet.containsKey(blockValue))
-                    blockSet.put(blockValue, new HashSet<Character>());
-                if(val != '.'){
-                    if(!blockSet.get(blockValue).contains(val)){
-                        blockSet.get(blockValue).add(val);
-                    } else
-                        return false;    
-                }
-            }
-        }
-        
         return true;
     }
 }
