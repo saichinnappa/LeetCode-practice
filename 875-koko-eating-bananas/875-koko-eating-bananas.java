99cd78935e96082da76525346881e564c7d8b2a1
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-12 19:03:14 -0500

    Time: 52 ms (11.11%), Space: 52.9 MB (72.21%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,5 +1,10 @@
+/**
+Initial search sapce 1 to m, takes log m comparisions to reduce search space to 1.
+For each eating speed middle, we traverse the array -> O(n)
+Overall O(n.logm)
+**/
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
+    public int minEatingSpeed(int[] orders, int hours) {
         //BRUTE FORCE - TLE
         
 //         int totalSpeed = 0;
@@ -25,38 +30,75 @@ class Solution {
         
         // BINARY SEARCH
         
-        int maxValue = Integer.MIN_VALUE;
-        for(int i = 0; i< piles.length; i++){
-            maxValue = Math.max(piles[i], maxValue);
-        }
-        int start = 1;
-        int end = maxValue;
+//         int maxValue = Integer.MIN_VALUE;
+//         for(int i = 0; i< piles.length; i++){
+//             maxValue = Math.max(piles[i], maxValue);
+//         }
+//         int start = 1;
+//         int end = maxValue;
         
-        int mid = (start + end) / 2;
-        int result = Integer.MAX_VALUE;
+//         int mid = (start + end) / 2;
+//         int result = Integer.MAX_VALUE;
         
-        while(start <= end){
-            int totalSpeed = 0;
+//         while(start <= end){
+//             int totalSpeed = 0;
 
-            for(int i = 0; i< piles.length; i++){
-                totalSpeed += Math.ceil((double)piles[i] / mid);
-                if(totalSpeed > h){
-                    start = mid + 1;
-                    break;
-                }
-            }
-            if(totalSpeed <= h){
-                end = mid - 1;
-                result = Math.min(mid, result);
+//             for(int i = 0; i< piles.length; i++){
+//                 totalSpeed += Math.ceil((double)piles[i] / mid);
+//                 if(totalSpeed > h){
+//                     start = mid + 1;
+//                     break;
+//                 }
+//             }
+//             if(totalSpeed <= h){
+//                 end = mid - 1;
+//                 result = Math.min(mid, result);
                 
-            }
+//             }
             
-            mid = (start + end)/2;
-            // 
-            totalSpeed = 0;
+//             mid = (start + end)/2;
+//             // 
+//             totalSpeed = 0;
+//         }
+        
+//         return result;
+        
+//     }
+        
+        
+        int left = 1, right = 1;
+
+        // find max orders for upper limit of search. You will never deliver more than max orders in any hour.
+        // You can also use Integer.MAX_VALUE
+        for (int order : orders) {
+            right = Math.max(right, order);
         }
-        
-        return result;
-        
+
+        // binary search
+        while (left < right) {
+
+            // We start with speed = middle; and then move left or right.
+            int middle = (left + right) / 2;
+
+            int hourSpent = 0;
+
+            // Iterate over the orders and calculate hourSpent.
+            // We increase the hourSpent by ceil (order / middle)
+            for (int order : orders) {
+                hourSpent += Math.ceil((double) order / middle);
+            }
+
+            // cut the search space by half based on whether the speed can be further optimized
+            if (hourSpent <= hours) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the minimum speed of delivery.
+        return right;
     }
+        
 }
