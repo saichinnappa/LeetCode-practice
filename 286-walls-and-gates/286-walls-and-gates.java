49e7d1e2077ae2fc9ef0aa576ab8cbbb03f41fa1
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-13 12:14:40 -0500

    Time: 13 ms (43.99%), Space: 56.8 MB (54.74%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -1,35 +1,42 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        Deque<Pair> queue = new LinkedList<>();
-        for(int i = 0; i < rooms.length; i++){
-            for(int j = 0; j < rooms[i].length; j++){
-                if(rooms[i][j] == 0){
-                    queue.offer(new Pair(i, j));
-                }
+        int m = rooms.length;
+        int n = rooms[0].length;
+        
+        Deque<Cell> queue = new LinkedList<>();
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n ; j++){
+                if(rooms[i][j] == 0)
+                    queue.offer(new Cell(i, j, 0));
             }
         }
-        int[][] directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        
+        int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         while(!queue.isEmpty()){
-            Pair first = queue.poll();
-            for(int[] dir : directions){
-                int newRow = first.row + dir[0];
-                int newCol = first.col + dir[1];
-                if(newRow > -1 && newCol > -1 && newRow < rooms.length && newCol < rooms[0].length && rooms[newRow][newCol] == Integer.MAX_VALUE){
-                    rooms[newRow][newCol] = rooms[first.row][first.col] + 1;
-                    queue.offer(new Pair(newRow, newCol));
+            Cell first = queue.poll();
+            for(int[] d : directions){
+                int newRow = first.row + d[0];
+                int newCol = first.col + d[1];
+                if(newRow > -1 && newCol > -1 && newRow < m && newCol < n && rooms[newRow][newCol] == Integer.MAX_VALUE){
+                    rooms[newRow][newCol] = first.dist + 1;
+                    queue.offer(new Cell(newRow, newCol, first.dist + 1));
                 }
             }
+            
         }
         
     }
 }
 
-class Pair{
+class Cell{
     int row;
     int col;
+    int dist;
     
-    Pair(int row, int col){
+    Cell(int row, int col, int dist){
         this.row = row;
         this.col = col;
+        this.dist = dist;
     }
 }
