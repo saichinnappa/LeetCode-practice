42250502e81083cef0197096fb62583e66852ae2
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-13 11:19:17 -0500

    Time: 195 ms (20.65%), Space: 119.9 MB (29.28%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -11,16 +11,6 @@ class TrieNode {
         }
 }
 
-class Path {
-        String path;
-        int value;
-
-        public Path(String path, int value) {
-            this.path = path;
-            this.value = value;
-        }
-}
-
 class FileSystem {
 
     TrieNode root;
@@ -100,8 +90,33 @@ class FileSystem {
 }
 
 /**
- * Your FileSystem object will be instantiated and called as such:
- * FileSystem obj = new FileSystem();
- * boolean param_1 = obj.createPath(path,value);
- * int param_2 = obj.get(path);
- */
+class FileSystem {
+
+    Map<String, Integer> fileSystem;
+    
+    public FileSystem() {
+        fileSystem = new HashMap<>();
+    }
+    
+    public boolean createPath(String path, int value) {
+        if(path.equals("")  || path.equals("/") || fileSystem.containsKey(path))
+            return false;
+        int lastIndex = path.lastIndexOf("/");
+        if(lastIndex == 0){
+            fileSystem.put(path, value);
+            return true;
+        }
+        String parent = path.substring(0, lastIndex);
+        if(fileSystem.containsKey(parent)){
+            fileSystem.put(path, value);
+            return true;
+        }
+        return false;
+    }
+    
+    public int get(String path) {
+        return fileSystem.containsKey(path) ? fileSystem.get(path) : -1;
+    }
+}
+
+**/
