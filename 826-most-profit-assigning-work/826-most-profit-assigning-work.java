1c464802a270d57d41eb17e5da08b8d7c33d5abf
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-13 11:43:30 -0500

    Time: 30 ms (63.55%), Space: 61.5 MB (48.91%) - LeetHub

--- 826-most-profit-assigning-work/826-most-profit-assigning-work.java
+++ 826-most-profit-assigning-work/826-most-profit-assigning-work.java
@@ -1,49 +1,27 @@
 class Solution {
-    List<Job> jobs = new ArrayList<>();
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int maxProfit = 0;
+        List<Job> jobList = new ArrayList<>();
         for(int i = 0; i < difficulty.length; i++){
             Job job = new Job(difficulty[i], profit[i]);
-            jobs.add(job);
+            jobList.add(job);
         }
-        
-        Collections.sort(jobs, (j1, j2) -> j1.difficulty - j2.difficulty);
+        jobList.sort((j1, j2) -> j1.difficulty - j2.difficulty);
         Arrays.sort(worker);
         
-        int n = jobs.size();
-        int x = 0;
+        int maxProfit = 0;
+        int n = jobList.size();
+        int j = 0;
         int best = 0;
         for(int i = 0; i < worker.length; i++){
-            while(x < n && jobs.get(x).difficulty <= worker[i]){
-                best = Math.max(best, jobs.get(x).profit);
-                x++;
+            while(j < n && jobList.get(j).difficulty <= worker[i]){
+                best = Math.max(best, jobList.get(j).profit);
+                j++;
             }
             maxProfit += best;
         }
-        
         return maxProfit;
-        
-        
     }
-    
-    
-    // int binarySearch(int start, int end, int value){
-    //     while(start < end){
-    //         int mid = (start + end) / 2;
-    //         if(jobs.get(mid).difficulty == value){
-    //             return jobs.get(mid).profit;
-    //         }
-    //         if(jobs.get(mid).difficulty > value){
-    //             end = mid - 1;
-    //         } else{
-    //             start = mid + 1;
-    //         }
-    //     }
-    //      return jobs.get(start).profit;
-    // }
-    
 }
-
 class Job{
     int difficulty;
     int profit;
