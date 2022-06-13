59611573bf15cf17c2971f27d27f11de59ee376b
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-13 11:18:31 -0500

    Time: 205 ms (17.26%), Space: 121.9 MB (5.15%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -62,6 +62,41 @@ class FileSystem {
         }
         return -1;
     }
+    
+    
+    
+    private void deletePath(String path){
+        String[] p = path.split("/");
+        TrieNode curr = root;
+        String[] p1 = new String[p.length - 1];
+        for(int i = 1; i < p.length; i++){
+            p1[i - 1] = p[i];
+        }
+        deleteNode(p1, 0, root);
+    }
+
+    static void deleteNode(String[] p1, int level, TrieNode node){
+        if(level == p1.length - 1){
+            TrieNode curr = node.children.get(p1[level]);
+            if(curr.children.size() == 0){
+                node.children.remove(p1[level]);
+                System.out.println(" deleted successfully ");
+                return;
+            } else{
+                System.out.println(" children exists, cannot delete node ");
+                return;
+            }
+        }
+
+        if(node.children.containsKey(p1[level])){
+            deleteNode(p1, level + 1, node.children.get(p1[level]));
+        } else{
+            System.out.println("Parent doesn't exist");
+        }
+
+    }
+    
+    
 }
 
 /**
