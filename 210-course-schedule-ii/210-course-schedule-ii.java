dd0ca87d332e1de8e7d758fd6ddcbb610c073d82
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-15 07:03:50 -0500

    Time: 23 ms (15.67%), Space: 43 MB (96.81%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,59 +1,72 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] arr) {
+//     public int[] findOrder(int numCourses, int[][] prerequisites) {
+//         int[] result = new int[numCourses];
+//         //Course pre-req calculation (in degree of a vertex in graph)
+//         int[] preReqCount = new int[numCourses];
+
+//         for (int[] prerequisite : prerequisites) {
+//             preReqCount[prerequisite[0]]++;
+//         }
+
+//         Queue<Integer> queue = new ArrayDeque<>();
+
+//         for (int i=0; i<numCourses; i++) {
+//             if ( preReqCount[i]== 0) {
+//                 queue.offer(i);
+//             }
+//         }
         
-        Map<Integer, List<Integer>> m = new HashMap<>();
-        for( int i = 0; i < arr.length; i++ ) {
-            m.putIfAbsent(arr[i][0], new LinkedList<Integer>());
-            for( int j = 1; j < arr[i].length; j++ ) {
-                m.get(arr[i][0]).add(arr[i][j]);
-            }
+//         // if(queue.isEmpty()) return new int[]{};
+
+//         int index = 0;
+//         while (!queue.isEmpty()) {
+//             int course = queue.poll();
+//             result[index] = course;
+//             index++;
+//             for(int[] pre : prerequisites){
+//                 if(pre[1] == course){
+//                     preReqCount[pre[0]]--;
+//                     if(preReqCount[pre[0]] == 0){
+//                         queue.offer(pre[0]);
+//                     }
+//                 }
+//             }
+            
+//         }
+
+//         return index == numCourses ? result : new int[0];
+//     }
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) { 
+    if (numCourses == 0) return null;
+    // Convert graph presentation from edges to indegree of adjacent list.
+    int indegree[] = new int[numCourses], order[] = new int[numCourses], index = 0;
+    for (int i = 0; i < prerequisites.length; i++) // Indegree - how many prerequisites are needed.
+        indegree[prerequisites[i][0]]++;    
+
+    Queue<Integer> queue = new LinkedList<Integer>();
+    for (int i = 0; i < numCourses; i++) 
+        if (indegree[i] == 0) {
+            // Add the course to the order because it has no prerequisites.
+            order[index++] = i;
+            queue.offer(i);
         }
-        
-        boolean[] visited = new boolean[numCourses];
-        Set<Integer> result = new LinkedHashSet<>();
-        
-        for( int course = 0; course < numCourses; course++ ) {
-            if( !getDependentCourse(course, m, visited, result) ) {
-                result.clear();
-                break;
-            }
+
+    // How many courses don't need prerequisites. 
+    while (!queue.isEmpty()) {
+        int prerequisite = queue.poll(); // Already finished this prerequisite course.
+        for (int i = 0; i < prerequisites.length; i++)  {
+            if (prerequisites[i][1] == prerequisite) {
+                indegree[prerequisites[i][0]]--; 
+                if (indegree[prerequisites[i][0]] == 0) {
+                    // If indegree is zero, then add the course to the order.
+                    order[index++] = prerequisites[i][0];
+                    queue.offer(prerequisites[i][0]);
+                }
+            } 
         }
-        
-        //System.out.println(m);
-        //System.out.println(result);
-        
-        return result.stream().mapToInt(i->i).toArray();
-        
     }
-    
-    boolean getDependentCourse(int course, Map<Integer, List<Integer>> m, boolean[] visited, Set<Integer> result) {
-    
-        if( result.contains(course) ) {
-            return true;
-        }
-        
-        if( ! m.containsKey(course) ) {
-            result.add(course);
-            return true;
-        }
-        
-        if ( visited[course] ) {
-            //cycle found in connection
-            return false;
-        }
-        
-        visited[course] = true;
-        
-        
-        for( int dc: m.get(course) ) {
-            if( !getDependentCourse(dc, m, visited, result) ) {
-                return false;
-            }
-        }
-        
-        visited[course] = false;
-        result.add(course);
-        return true;
-    }
-    
+
+    return (index == numCourses) ? order : new int[0];
+}
 }
