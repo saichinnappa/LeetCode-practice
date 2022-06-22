6eabca842449713d2dcc05a9af67767898816388
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-22 08:48:39 -0500

    Time: 8 ms (51.52%), Space: 44.5 MB (39.26%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,34 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.length() == 0)
+        if(s.length() == 1)
             return true;
+        
         char[] arr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < arr.length; i++){
-            if(Character.isDigit(arr[i]))
-                sb.append(arr[i]);
-            if(Character.isLetter(arr[i])){
-                if(Character.isUpperCase(arr[i])){
-                    sb.append(Character.toLowerCase(arr[i]));    
-                } else
-                    sb.append(arr[i]);
+        for(char c : arr){
+            if(Character.isLetter(c) || Character.isDigit(c)){
+                sb.append(Character.toLowerCase(c));
             }
         }
         
-        return checkPalindrom(sb.toString());
-    }
-    
-    boolean checkPalindrom(String s){
-        int start = 0;
-        int end = s.length() - 1;
+        String newS = sb.toString();
+        
+        int start = 0, end = newS.length() - 1;
+        
         while(start <= end){
-            if(s.charAt(start) == s.charAt(end)){
+            if(newS.charAt(start) == newS.charAt(end)){
                 start++;
                 end--;
             } else{
                 return false;
             }
         }
-        return true;
+     return true;   
     }
 }
