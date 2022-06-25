38a33a22d657d918c86dc8dffc93cb2f48ce9b90
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 09:55:15 -0500

    Time: 2 ms (11.60%), Space: 42.1 MB (66.39%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,52 +15,30 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)   
+        if(root == null)
+            return true;
+        if(root.left == null && root.right != null)
             return false;
-        // return helper(root.left, root.right);
-        // if(root.left == null && root.right != null)
-        //     return false;
-        // if(root.left != null && root.right == null)
-        //     return false;
-        // if(root.left.val != root.right.val)
-        //     return false;
-        Deque<TreeNode> queue = new LinkedList<>();
+        if(root.left != null && root.right == null)
+            return false;
+        Deque<TreeNode> queue  = new LinkedList<TreeNode>();
         queue.offer(root.left);
         queue.offer(root.right);
         while(!queue.isEmpty()){
             TreeNode t1 = queue.poll();
             TreeNode t2 = queue.poll();
-            
-            if(t1 == null && t2 != null)
+            if((t1 != null && t2 != null) && (t1.val != t2.val))
                 return false;
-            if(t1 != null && t2 == null)
+            if((t1 == null && t2 != null) || (t1 != null && t2 == null))
                 return false;
-            if(t1 == null && t2 ==null)
+            if(t1 == null && t2 == null)
                 continue;
-            if(t1.val != t2.val)
-                return false;
-            
             queue.offer(t1.left);
             queue.offer(t2.right);
             queue.offer(t1.right);
             queue.offer(t2.left);
-        
         }
         
         return true;
-        
-    }
-    
-    boolean helper(TreeNode t1, TreeNode t2){
-        if(t1 == null && t2 == null)
-            return true;
-        if(t1 == null && t2 != null)
-            return false;
-        if(t1 != null && t2 == null)
-            return false;
-        if(t1.val != t2.val)
-            return false;
-            
-        return helper(t1.left, t2.right) && helper(t1.right,  t2.left);
     }
 }
