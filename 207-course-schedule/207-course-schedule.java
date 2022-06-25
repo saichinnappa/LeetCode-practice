821cb0d0073ffa96e4cd8fce00f9a5e53f90c94f
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 15:10:58 -0500

    Time: 10 ms (46.30%), Space: 48.8 MB (37.65%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -2,17 +2,16 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> map = new HashMap<>();
         int[] courses = new int[numCourses];
+        
         for(int[] pre : prerequisites){
             courses[pre[0]]++;
             map.putIfAbsent(pre[1], new ArrayList<>());
             map.get(pre[1]).add(pre[0]);
         }
         
-        Deque<Integer> queue = new LinkedList<Integer>();
-        
-        for(int i = 0; i < courses.length; i++){
+        Deque<Integer> queue = new LinkedList<>();
+        for(int i =0; i < courses.length; i++){
             if(courses[i] == 0){
-                // System.out.println(i);
                 queue.offer(i);
             }
         }
@@ -20,24 +19,24 @@ class Solution {
         if(queue.isEmpty())
             return false;
         
-        // System.out.println(Arrays.toString(courses));
-        
         while(!queue.isEmpty()){
             int first = queue.poll();
-            // System.out.println(first);
             if(map.containsKey(first)){
                 List<Integer> list = map.get(first);
-                for(int i = 0; i < list.size(); i++){
-                    courses[list.get(i)]--;
-                    if(courses[list.get(i)] == 0)
-                        queue.offer(list.get(i));
-                }    
+                for(int c : list){
+                    courses[c]--;
+                    if(courses[c] == 0)
+                        queue.offer(c);
+                }
             }
         }
+        
         for(int c : courses){
             if(c > 0)
                 return false;
         }
+        
         return true;
+            
     }
 }
