837aff369b357c451907b2d1f70b32fd47816c60
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 17:59:35 -0500

    Time: 1 ms (71.09%), Space: 42.6 MB (45.84%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,81 +1,58 @@
 class Solution {
-    int[] nums;
-    int target;
-    int length;
     public int search(int[] nums, int target) {
-        this.length = nums.length ;
-        this.nums = nums;
-        this.target = target;
         int start = 0;
         int end = nums.length - 1;
-                if(nums.length == 1){
+        
+         if(nums.length == 1){
             if(nums[0] == target)
                 return 0;
             else
                 return -1;
         }
+        
         if(nums[start] < nums[end]){
-            return binarySearch(start, end);
+            return binarySearch(nums, start, end, target);
         }
         
-        int rIdx = rotatedIndex(start, end);
+        int rIdx = rotatedIdx(nums, start, end);
         if(nums[rIdx] == target)
             return rIdx;
-        if(nums[end] >= target)
-            return binarySearch(rIdx + 1, end);
-        if(nums[end] < target)
-            return binarySearch(start, rIdx - 1);
+        if(nums[end] < target){
+            return binarySearch(nums, start, rIdx - 1, target);
+        } else{
+            return binarySearch(nums, rIdx + 1, end, target);
+        }
         
-            return -1;
-            
-            
+        
     }
     
-    
-    int binarySearch(int start, int end){
+    int rotatedIdx(int[] nums, int start, int end){
         while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
+            int mid = (start + end) /2;
+            if(nums[mid] > nums[mid + 1])
+                return mid + 1;
+            if(nums[mid] < nums[mid - 1])
                 return mid;
-            if(nums[mid] > target){
-                end = mid - 1;
-            } else{
+            if(nums[mid] > nums[start]){
                 start = mid + 1;
+            } else if(nums[mid] < nums[end]){
+                end = mid - 1;
             }
         }
         return -1;
     }
     
-    // int rotatedIndex(int start, int end){
-    //     while(start <= end){
-    //         int mid = start + (end - start) / 2;
-    //         if(nums[mid] > nums[mid + 1])
-    //             return mid + 1;
-    //         if(nums[mid] < nums[mid - 1])
-    //             return mid;
-    //         if(nums[mid] > nums[start])
-    //             start = mid + 1;
-    //         else if (nums[mid] < nums[end])
-    //             end = mid - 1;
-    //     }
-    //     return -1;
-    // }
     
-       int rotatedIndex(int start, int end){
+    int binarySearch(int[] nums, int start, int end, int target){
         while(start <= end){
-            int mid = (start) + (end - start) /2 ;
-            if(nums[mid] > nums[mid + 1])
-                return mid + 1;
-            else if(nums[mid] < nums[mid - 1])
-                return mid;
-            else if (nums[mid] > nums[start]){
-                start = mid + 1;
-            } else if(nums[mid] < nums[end])
+            int mid = (start + end) / 2;
+            if(nums[mid] > target)
                 end = mid - 1;
+           else if(nums[mid] < target)
+                start = mid + 1;
+            else
+                return mid;
         }
         return -1;
     }
-    
-    
-    
 }
