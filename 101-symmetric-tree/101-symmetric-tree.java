879714f46c46d74605c29826fa60d22d0c003a09
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 09:56:19 -0500

    Time: 2 ms (11.60%), Space: 42.9 MB (10.53%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -27,12 +27,14 @@ class Solution {
         while(!queue.isEmpty()){
             TreeNode t1 = queue.poll();
             TreeNode t2 = queue.poll();
-            if((t1 != null && t2 != null) && (t1.val != t2.val))
-                return false;
-            if((t1 == null && t2 != null) || (t1 != null && t2 == null))
-                return false;
             if(t1 == null && t2 == null)
                 continue;
+            if((t1 == null && t2 != null) || (t1 != null && t2 == null))
+                return false;
+            if(t1.val != t2.val)
+                return false;
+
+
             queue.offer(t1.left);
             queue.offer(t2.right);
             queue.offer(t1.right);
