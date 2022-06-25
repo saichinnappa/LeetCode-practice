3a213aea65c9fbc27272a594b021b709372cbc2a
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 16:46:57 -0500

    Time: 1 ms (58.83%), Space: 42.1 MB (84.39%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -17,8 +17,30 @@ class Solution {
     TreeNode pre = null;
     boolean isValid = true;
     public boolean isValidBST(TreeNode root) {
-        helper(root);
-        return isValid;
+        // helper(root);
+        // return isValid;
+        
+        if(root == null)
+            return false;
+        
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode curr = root;
+        TreeNode prev = null;
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            TreeNode top = stack.pop();
+            if(prev != null){
+                if(prev.val >= top.val)
+                    return false;
+            }
+            prev = top;
+            curr = top.right;
+        }
+        return true;
+        
     }
     
     void helper(TreeNode root){
