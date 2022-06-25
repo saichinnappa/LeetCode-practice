9c49a0813feb68f67e48875a04f0e73f97d51fb0
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-25 10:00:49 -0500

    Time: 1 ms (58.93%), Space: 43.1 MB (7.23%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,33 +14,49 @@
  * }
  */
 class Solution {
+    boolean isValid = true;
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-            return true;
-        if(root.left == null && root.right != null)
-            return false;
-        if(root.left != null && root.right == null)
-            return false;
-        Deque<TreeNode> queue  = new LinkedList<TreeNode>();
-        queue.offer(root.left);
-        queue.offer(root.right);
-        while(!queue.isEmpty()){
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            if(t1 == null && t2 == null)
-                continue;
-            if((t1 == null && t2 != null) || (t1 != null && t2 == null))
-                return false;
-            if(t1.val != t2.val)
-                return false;
+//         if(root == null)
+//             return true;
+//         if(root.left == null && root.right != null)
+//             return false;
+//         if(root.left != null && root.right == null)
+//             return false;
+//         Deque<TreeNode> queue  = new LinkedList<TreeNode>();
+//         queue.offer(root.left);
+//         queue.offer(root.right);
+//         while(!queue.isEmpty()){
+//             TreeNode t1 = queue.poll();
+//             TreeNode t2 = queue.poll();
+//             if(t1 == null && t2 == null)
+//                 continue;
+//             if((t1 == null && t2 != null) || (t1 != null && t2 == null))
+//                 return false;
+//             if(t1.val != t2.val)
+//                 return false;
 
-
-            queue.offer(t1.left);
-            queue.offer(t2.right);
-            queue.offer(t1.right);
-            queue.offer(t2.left);
+//             queue.offer(t1.left);
+//             queue.offer(t2.right);
+//             queue.offer(t1.right);
+//             queue.offer(t2.left);
+//         }
+        
+//         return true;
+        helper(root.left, root.right);
+        return isValid;
+    }
+    void helper(TreeNode left, TreeNode right){
+        if(left == null && right != null)
+            isValid = false;
+        if(left != null && right == null)
+            isValid = false;
+        if((left != null && right != null) && (left.val != right.val))
+            isValid = false;
+        if(left != null && right != null){
+            helper(left.left, right.right);
+        helper(left.right, right.left);    
         }
         
-        return true;
+            
     }
 }
