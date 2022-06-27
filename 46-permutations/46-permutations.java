68e8fe22554383e260aaed47f263630ecf6720a3
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-26 19:04:08 -0500

    Time: 6 ms (8.91%), Space: 45.8 MB (5.23%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,70 +1,36 @@
-// class Solution {
-//     Set<Integer> set = new HashSet<Integer>();
-//     List<List<Integer>> result = new ArrayList<>();
-//     int[] nums;
-//     public List<List<Integer>> permute(int[] nums) {
-//         this.nums = nums;
-//         solve(new ArrayList());
-//         return result;
-//     }
-    
-//     List<Integer> getCandidates(){
-//         List<Integer> list = new ArrayList<>();
-//         for(int n : nums){
-//             if(!set.contains(n))
-//                 list.add(n);
-//         }
-//         return list;
-//     }
-    
-    
-//     void solve(List<Integer> list){
-//         if(list.size() == nums.length){
-//             result.add(new ArrayList<>(list));
-//         }
-        
-//         List<Integer> candidates = getCandidates();
-        
-//         for(int n : candidates){
-//             set.add(n);
-//             list.add(n);
-//             solve(list);
-//             set.remove(n);
-//             list.remove(list.size() - 1);
-//         }
-//     }
-// }
-
 class Solution {
-  public void backtrack(int n,
-                        ArrayList<Integer> nums,
-                        List<List<Integer>> output,
-                        int first) {
-    // if all integers are used up
-    if (first == n)
-      output.add(new ArrayList<Integer>(nums));
-    for (int i = first; i < n; i++) {
-      // place i-th integer first 
-      // in the current permutation
-      Collections.swap(nums, first, i);
-      // use next integers to complete the permutations
-      backtrack(n, nums, output, first + 1);
-      // backtrack
-      Collections.swap(nums, first, i);
+    Set<Integer> set = new HashSet<Integer>();
+    List<List<Integer>> result = new ArrayList<>();
+    int[] nums;
+    public List<List<Integer>> permute(int[] nums) {
+        this.nums = nums;
+        solve(new ArrayList());
+        return result;
     }
-  }
-
-  public List<List<Integer>> permute(int[] nums) {
-    // init output list
-    List<List<Integer>> output = new LinkedList();
-
-    // convert nums into list since the output is a list of lists
-    ArrayList<Integer> nums_lst = new ArrayList<Integer>();
-    for (int num : nums)
-      nums_lst.add(num);
-
-    int n = nums.length;
-    backtrack(n, nums_lst, output, 0);
-    return output;
-  }
+    
+    List<Integer> getCandidates(){
+        List<Integer> list = new ArrayList<>();
+        for(int n : nums){
+            if(!set.contains(n))
+                list.add(n);
+        }
+        return list;
+    }
+    
+    
+    void solve(List<Integer> list){
+        if(list.size() == nums.length){
+            result.add(new ArrayList<>(list));
+        }
+        
+        List<Integer> candidates = getCandidates();
+        
+        for(int n : candidates){
+            set.add(n);
+            list.add(n);
+            solve(list);
+            set.remove(n);
+            list.remove(list.size() - 1);
+        }
+    }
 }
