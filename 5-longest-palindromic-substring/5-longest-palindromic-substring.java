e8eb1aa7de564a549afe1d9d378aa5c756472ae2
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-29 07:11:37 -0500

    Time: 207 ms (37.94%), Space: 87 MB (18.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -3,37 +3,36 @@ class Solution {
         int length = s.length();
         if(length == 1)
             return s;
+        char[] arr = s.toCharArray();
+        int maxLength= 0;
+        int start = 0;
         boolean[][] dp = new boolean[length][length];
-        int start = -1;
-        int size = -1;
-        for(int i = 0; i < length; i++){
+        
+        for(int i = 0; i < arr.length; i++){
             dp[i][i] = true;
+            maxLength = 1;
             start = i;
-            size = 1;
         }
         
-        for(int i = 0; i < length - 1; i++){
-            if(s.charAt(i) == s.charAt(i + 1)){
-                dp[i][i + 1] = true;
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] == arr[i+1]){
+                dp[i][i+1] = true;
+                maxLength = 2;
                 start = i;
-                size = 2;
             }
         }
         
-        for(int k = 3; k <= length; k++){
-            for(int i = 0; i < length - k + 1; i++){
-                int j = k  + i - 1;
-                // System.out.println(i+"--"+j+" "+ k);
-                if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]){
+        for(int k = 3;k <= length; k++){
+            for(int i = 0; i < length -k + 1; i++){
+                int j = k + i - 1;
+                if(arr[i] == arr[j] && dp[i+1][j-1]){
                     dp[i][j] = true;
-                    if(size < k){
-                        start = i;
-                        size = k;
-                    }
+                    maxLength = k;
+                    start = i;
                 }
             }
         }
-        // System.out.println(start+" "+ size);
-        return size == -1 ? "" : s.substring(start, start + size);
+        
+        return s.substring(start, start + maxLength);
     }
 }
