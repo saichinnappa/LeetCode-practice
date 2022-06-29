ffea6eb415b34421912542972c93f6c9473cfb90
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-29 07:04:35 -0500

    Time: 274 ms (35.81%), Space: 46.5 MB (49.73%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,40 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length() == 1)
+        int length = s.length();
+        if(length == 1)
             return s;
-        int n = s.length();
-        char[] arr = s.toCharArray();
-        boolean[][] dp = new boolean[n][n];
-        int start = 0;
-        int length = 0;
-        //length 1
-        for(int i = 0; i < arr.length; i++){
+        boolean[][] dp = new boolean[length][length];
+        int start = -1;
+        int size = -1;
+        for(int i = 0; i < length; i++){
             dp[i][i] = true;
             start = i;
-            length = 1;
+            size = 1;
         }
         
-        //length 2;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] == arr[i + 1]){
+        for(int i = 0; i < length - 1; i++){
+            if(s.charAt(i) == s.charAt(i + 1)){
                 dp[i][i + 1] = true;
                 start = i;
-                length = 2;
+                size = 2;
             }
         }
         
-        //length >= 3
-        for(int k = 3; k <= n ; k++){
-            for(int i = 0 ; i < n - k + 1; i++){
-                int j = i + k - 1;
-                if(arr[i] == arr[j] && dp[i + 1][j - 1]){
+        for(int k = 3; k <= length; k++){
+            for(int i = 0; i < length - k + 1; i++){
+                int j = k  + i - 1;
+                // System.out.println(i+"--"+j+" "+ k);
+                if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]){
                     dp[i][j] = true;
-                    start = i;
-                    length = k;
+                    if(size < k){
+                        start = i;
+                        size = k;
+                    }
                 }
             }
         }
-        
-        return s.substring(start, start + length);
+        // System.out.println(start+" "+ size);
+        return size == -1 ? "" : s.substring(start, start + size);
     }
 }
