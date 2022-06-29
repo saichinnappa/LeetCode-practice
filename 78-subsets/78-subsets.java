b5c1edba9708d0a99fb114b9ce5ae202bf19561f
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-29 18:55:06 -0500

    Time: 1 ms (88.31%), Space: 42.4 MB (87.75%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,53 +1,19 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-
     public List<List<Integer>> subsets(int[] nums) {
-        
-        for(int k = 0; k < nums.length + 1; k++){
-            solve(nums, new ArrayList<>(), k , 0);
+        for(int i = 0; i < nums.length +1; i++){
+            solve(i, nums, new ArrayList<>(), 0);
         }
         return result;
     }
     
-    void solve(int[] nums, List<Integer> list, int k,  int idx){
-        if(list.size() == k){
+    void solve(int k, int[] nums, List<Integer> list, int idx){
+        if(list.size() == k)
             result.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i = idx; i < nums.length; i++){
+        for(int i =idx; i < nums.length; i++){
             list.add(nums[i]);
-            // System.out.println(list);
-            solve(nums, list, k, i + 1);
-            list.remove(list.size()  - 1);
+            solve(k, nums, list, i+ 1);
+            list.remove(list.size() -1);
         }
     }
 }
-
-// class Solution {
-//     // int n, k;
-//     int[] nums;
-//     List<List<Integer>> result = new ArrayList();
-//     public List<List<Integer>> subsets(int[] nums) {
-//         this.nums = nums;
-//         // this.n = nums.length;
-//         for(int k = 0; k < nums.length + 1; k++){
-//             backtrack(0, new ArrayList<>(),k);
-//         }
-//         return result;
-//     }
-    
-//     void backtrack(int first, List<Integer> list,int k){
-//         if(list.size() == k){
-//             result.add(new ArrayList(list));
-//             return;
-//         }
-        
-//         for(int i = first; i< nums.length; i++){
-//             list.add(nums[i]);
-//             backtrack(i + 1, list,  k);
-//             list.remove(list.size() - 1);
-//         }
-//     }
-// }
-    
-// }
