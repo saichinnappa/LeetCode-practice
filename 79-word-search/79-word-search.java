c39fd3261edeeffa29ef8e5c2b3e572611f839d5
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-30 07:58:10 -0500

    Time: 201 ms (35.47%), Space: 42.2 MB (48.85%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,40 +1,36 @@
 class Solution {
-    boolean exists = false;
     boolean[][] visited;
     char[][] board;
+    boolean found = false;
     public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        this.visited = new boolean[m][n];
         this.board = board;
-        visited = new boolean[board.length][board[0].length];
-        for(int i =0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n ; j++){
                 if(board[i][j] == word.charAt(0)){
-                    dfs(i, j, 0, word);
-                    if(exists)
+                    dfs(i, j, m, n, 0, word);
+                    if(found)
                         return true;
                 }
             }
         }
+            
         return false;
     }
     
-    
-    void dfs(int row, int col, int index, String word){
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || index >= word.length() || visited[row][col] || word.charAt(index) != board[row][col]){
-            return;
-        }
-        if(word.charAt(index) == board[row][col]){
+    void dfs(int row, int col, int m, int n, int idx, String word){
+        if(row > -1 && col > -1 && row < m && col < n && !visited[row][col] && idx < word.length() && board[row][col] == word.charAt(idx)){
             visited[row][col] = true;
-            if(index == word.length() - 1){
-                exists = true;
-                return;
-            }
+            if(idx == word.length() - 1)
+                found = true;
+            dfs(row + 1, col, m, n, idx + 1, word);
+            dfs(row, col + 1, m, n, idx + 1, word);
+            dfs(row - 1, col, m, n, idx + 1, word);
+            dfs(row, col - 1, m, n, idx + 1, word);
+            visited[row][col] = false;
         }
-        
-        dfs(row + 1, col, index + 1, word);
-        dfs(row - 1, col, index + 1, word);
-        dfs(row, col + 1, index + 1, word);
-        dfs(row, col - 1, index + 1, word);
-        visited[row][col] = false;
+
     }
-    
 }
