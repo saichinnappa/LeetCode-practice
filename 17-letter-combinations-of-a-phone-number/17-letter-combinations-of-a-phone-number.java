50ca7f9e1da79e46745ac772c495354dcb027459
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-06-30 06:29:08 -0500

    Time: 2 ms (64.26%), Space: 42.4 MB (72.07%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,52 +1,41 @@
 class Solution {
-    List<String> result = new ArrayList();
     public List<String> letterCombinations(String digits) {
-        if(digits == "" || digits.length() == 0)
+        Map<String, String[]> map = new HashMap<>();
+        char[] arr = digits.toCharArray();
+        List<String> result = new ArrayList<String>();
+        if(arr.length == 0)
             return result;
         
-        Map<Integer, List<String>> map = new HashMap();
-        map.put(2, Arrays.asList("a", "b", "c"));
-        map.put(3, Arrays.asList("d", "e", "f"));
-        map.put(4, Arrays.asList("g", "h", "i"));
-        map.put(5, Arrays.asList("j", "k", "l"));
-        map.put(6, Arrays.asList("m", "n", "o"));
-        map.put(7, Arrays.asList("p", "q", "r", "s"));
-        map.put(8, Arrays.asList("t", "u", "v"));
-        map.put(9, Arrays.asList("w", "x", "y", "z"));
+        map.put("2", new String[]{"a", "b", "c"});
+        map.put("3", new String[]{"d", "e", "f"});
+        map.put("4", new String[]{"g", "h", "i"});
+        map.put("5", new String[]{"j", "k", "l"});
+        map.put("6", new String[]{"m", "n", "o"});
+        map.put("7", new String[]{"p", "q", "r", "s"});
+        map.put("8", new String[]{"t", "u", "v"});
+        map.put("9", new String[]{"w", "x", "y", "z"});
         
-        if(digits.length() == 1) return map.get(digits.charAt(0) - '0');
-        solve(map.get(digits.charAt(0) - '0'), digits, 1, map);
-        return result;
-        
-    }
-    
-    void solve(List<String> temp, String digits, int index, Map<Integer, List<String>> map){
-        if(index == digits.length()) return;
-        // temp = new ArrayList();
-        List<String> l = new ArrayList<>(temp);
-        List<String> list = map.get(digits.charAt(index) - '0');
-
-        if(index == digits.length() - 1){
-          for(String c : l){
-                for(String c1 : list){
-                    // c +=c1;
-                    String v = c + c1;
-                    if( v.length() == digits.length())
-                    result.add(c+c1);
+        for(int i = 0; i < arr.length; i++){
+            String[] sarr = map.get(String.valueOf(arr[i]));
+            if(result.size() == 0){
+                for(String s : sarr){
+                    result.add(s);
                 }
-            }
-        } else if(index >= 1){
-            for(String c : temp){
-                for(String c1 : list){
-                    // c +=c1;
-                    // System.out.println(c+c1);
-                    l.add(c+c1);
-                    
+            } else{
+                int idx = 0;
+                List<String> temp = new ArrayList<>(result);
+                int size = temp.size();
+                result = new ArrayList<String>();
+                while(size != 0){
+                    for(String s: sarr){
+                        result.add(temp.get(idx) + s);
+                    }
+                    size--;
+                    idx++;
                 }
             }
         }
         
-        index++;
-        solve(l, digits, index, map);
+        return result;
     }
 }
