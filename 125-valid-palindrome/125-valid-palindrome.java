1c670cf2304f833a5623cfe243af455792cfa26e
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-02 11:01:10 -0500

    Time: 5 ms (75.79%), Space: 42.7 MB (89.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,28 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.length() == 1)
+        if(s.trim().length() == 0)
             return true;
-        
         char[] arr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(char c : arr){
-            if(Character.isLetter(c) || Character.isDigit(c)){
-                sb.append(Character.toLowerCase(c));
+        for(int i = 0; i < arr.length; i++){
+            if(Character.isLetter(arr[i]) || Character.isDigit(arr[i])){
+                sb.append(arr[i]);
             }
         }
+        // System.out.println(sb.toString());
+        return isValid(sb.toString().toLowerCase());
         
-        String newS = sb.toString();
-        
-        int start = 0, end = newS.length() - 1;
-        
-        while(start <= end){
-            if(newS.charAt(start) == newS.charAt(end)){
+    }
+    
+    boolean isValid(String s){
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end){
+            if(s.charAt(start) == s.charAt(end)){
                 start++;
                 end--;
             } else{
                 return false;
             }
         }
-     return true;   
+        return true;
     }
 }
