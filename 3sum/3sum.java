c6b50dc41e40776c54fe13f24b3e446e2a62103b
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-02 08:09:56 -0500

    Time: 43 ms, Memory: 59 MB - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,38 +1,37 @@
 class Solution {
+    List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums.length == 0)
-            return result;
         Arrays.sort(nums);
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length - 1; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
             if(!set.contains(nums[i])){
-                int j = i + 1;
-                int k = nums.length - 1;
-                
+                set.add(nums[i]);
                 while(j < k){
-                    List<Integer> list = new ArrayList<>();
                     int sum = nums[i] + nums[j] + nums[k];
                     if(sum == 0){
-                        list.addAll(Arrays.asList(nums[i], nums[j], nums[k]));
-                        while(j < n - 1 && nums[j] == nums[j + 1]){
-                            j++;
+                        list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while(j < k){
+                            if((j + 1) < nums.length && nums[j] == nums[j + 1]){
+                                j++;
+                            } else{
+                                break;
+                            }
                         }
                         j++;
                         k--;
-                    } else if(sum > 0){
+                    }
+                    else if (sum > 0){
                         k--;
-                    } else{
+                    } else {
                         j++;
                     }
-                if(list.size() > 0)
-                    result.add(list);
-                }
-                set.add(nums[i]);
 
+                }
             }
         }
-        return result;
+        return list;
+        
     }
 }
