5d14c8451d0596172cd3fd3e3032925a94c91387
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-05 07:57:30 -0500

    Time: 1 ms (77.81%), Space: 43 MB (50.90%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -12,23 +12,36 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null) return list2;
         if(list2 == null) return list1;
+
+//          Iterative        
+//         ListNode head = new ListNode();
+//         ListNode tail = head;
         
-        ListNode head = new ListNode();
-        ListNode tail = head;
+//         while(list1 != null && list2 != null){
+//             if(list1.val < list2.val){
+//                 tail.next = new ListNode(list1.val);
+//                 list1 = list1.next;
+//             } else {
+//                 tail.next = new ListNode(list2.val);
+//                 list2 = list2.next;
+//             }
+//             tail = tail.next;
+//         }
         
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                tail.next = new ListNode(list1.val);
-                list1 = list1.next;
-            } else {
-                tail.next = new ListNode(list2.val);
-                list2 = list2.next;
-            }
-            tail = tail.next;
+//         tail.next = (list1 == null) ? list2 : list1;
+        
+//       return head.next;
+        
+        //Recursive
+        ListNode head;
+        if(list1.val < list2.val){
+            head = list1;
+            list1 = list1.next;
+        } else{
+            head = list2;
+            list2 = list2.next;
         }
-        
-        tail.next = (list1 == null) ? list2 : list1;
-        
-      return head.next;
+        head.next = mergeTwoLists(list1, list2);
+        return head;
     }
 }
