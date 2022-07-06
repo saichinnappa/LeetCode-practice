856fa07a3cf3079508527e30399f9dfe3dcdec90
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-06 12:56:45 -0500

    Time: 9 ms, Memory: 45.2 MB - LeetHub

---
+++ combination-sum/combination-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    int[] candidates;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.candidates = candidates;
+        solve(0, target, 0, new ArrayList<>());
+        return result;
+    }
+    
+    void solve(int index, int target, int sum, List<Integer> list){
+        if(sum == target){
+            result.add(new ArrayList<>(list));
+            // return;
+        }
+        for(int i = index; i< candidates.length; i++){
+            sum += candidates[i];
+            list.add(candidates[i]);
+            if(sum <= target){
+                solve(i, target, sum, list);
+            }
+            sum -= candidates[i];
+            list.remove(list.size() - 1);    
+            
+            
+        }
+    }
+}
