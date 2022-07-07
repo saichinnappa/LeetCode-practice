da8721f56129acaa907941cbedd4dd9a3302033c
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-06 22:38:31 -0500

    Time: 4 ms (85.08%), Space: 42.2 MB (95.26%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
             return false;
-        
+        int length = s.length();
         int[] arr = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            arr[s.charAt(i) - 'a']++;
+        for(int i = 0; i < length; i++){
+            arr[s.charAt(i)  -'a']++;
             arr[t.charAt(i) - 'a']--;
         }
-        
-        for(int i : arr){
-            if(i != 0)
+        for(int n : arr){
+            if(n != 0)
                 return false;
         }
         return true;
