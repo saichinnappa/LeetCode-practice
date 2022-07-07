8b7a9ed564028fac72245ff73b7115989855e6f3
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-07 18:05:49 -0500

    Time: 5 ms (47.20%), Space: 43.7 MB (68.15%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,40 +1,29 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList();
-        // Set<Integer> set = new HashSet();
-        // for(int i = 0; i< nums1.length; i++){
-        //     for(int j = 0; j < nums2.length; j++){
-        //         if(nums1[i] == nums2[j] && !set.contains(j)){
-        //             list.add(nums2[j]);
-        //             set.add(j);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return list.stream().mapToInt(i -> i).toArray();
-        
-        
-        //Sorting
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        
-        int i = 0, j =0;
-        
-        while(i != n1 && j!=n2){
-            if(nums1[i] == nums2[j]){
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i != nums1.length && j != nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            } else if(nums1[i] > nums2[j]){
+                j++;
+            } else{
+                
                 list.add(nums1[i]);
                 i++;
                 j++;
-            } else if (nums1[i] < nums2[j]){
-                i++;
-            } else{
-                j++;
             }
         }
-        
-        return list.stream().mapToInt(k -> k).toArray();
+        int[] result = new int[list.size()];
+        for(int k = 0; k < list.size(); k++){
+            
+            result[k] = list.get(k);
+            
+        }
+         
+        return result;
     }
 }
