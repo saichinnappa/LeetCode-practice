f296aa8d80685fcdc7796d8879f3bd89abf3a2eb
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-11 10:16:29 -0500

    Time: 18 ms (22.35%), Space: 49.5 MB (46.84%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,72 +1,39 @@
 class Solution {
-//     public int[] findOrder(int numCourses, int[][] prerequisites) {
-//         int[] result = new int[numCourses];
-//         //Course pre-req calculation (in degree of a vertex in graph)
-//         int[] preReqCount = new int[numCourses];
-
-//         for (int[] prerequisite : prerequisites) {
-//             preReqCount[prerequisite[0]]++;
-//         }
-
-//         Queue<Integer> queue = new ArrayDeque<>();
-
-//         for (int i=0; i<numCourses; i++) {
-//             if ( preReqCount[i]== 0) {
-//                 queue.offer(i);
-//             }
-//         }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        int[] count = new int[numCourses];
+        for(int[] pre : prerequisites){
+            map.putIfAbsent(pre[1], new ArrayList<>());
+            map.get(pre[1]).add(pre[0]);
+            count[pre[0]]++;
+        }
         
-//         // if(queue.isEmpty()) return new int[]{};
-
-//         int index = 0;
-//         while (!queue.isEmpty()) {
-//             int course = queue.poll();
-//             result[index] = course;
-//             index++;
-//             for(int[] pre : prerequisites){
-//                 if(pre[1] == course){
-//                     preReqCount[pre[0]]--;
-//                     if(preReqCount[pre[0]] == 0){
-//                         queue.offer(pre[0]);
-//                     }
-//                 }
-//             }
-            
-//         }
-
-//         return index == numCourses ? result : new int[0];
-//     }
-    
-    public int[] findOrder(int numCourses, int[][] prerequisites) { 
-    if (numCourses == 0) return null;
-    // Convert graph presentation from edges to indegree of adjacent list.
-    int indegree[] = new int[numCourses], order[] = new int[numCourses], index = 0;
-    for (int i = 0; i < prerequisites.length; i++) // Indegree - how many prerequisites are needed.
-        indegree[prerequisites[i][0]]++;    
-
-    Queue<Integer> queue = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) 
-        if (indegree[i] == 0) {
-            // Add the course to the order because it has no prerequisites.
-            order[index++] = i;
-            queue.offer(i);
+        Deque<Integer> queue = new LinkedList<>();
+        
+        for(int i = 0; i < count.length; i++){
+            if(count[i] == 0){
+                list.add(i);
+                queue.offer(i);
+            }
         }
 
-    // How many courses don't need prerequisites. 
-    while (!queue.isEmpty()) {
-        int prerequisite = queue.poll(); // Already finished this prerequisite course.
-        for (int i = 0; i < prerequisites.length; i++)  {
-            if (prerequisites[i][1] == prerequisite) {
-                indegree[prerequisites[i][0]]--; 
-                if (indegree[prerequisites[i][0]] == 0) {
-                    // If indegree is zero, then add the course to the order.
-                    order[index++] = prerequisites[i][0];
-                    queue.offer(prerequisites[i][0]);
+        if(queue.isEmpty())
+            return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        while(!queue.isEmpty()){
+            int first = queue.poll();
+            if(map.containsKey(first)){
+                List<Integer> l = map.get(first);
+                for(int c : l){
+                    count[c]--;
+                    if(count[c] == 0){
+                        list.add(c);
+                        queue.offer(c);
+                    }
                 }
-            } 
+            }
         }
+        return list.size() == numCourses ? list.stream().mapToInt(Integer::intValue).toArray() : new int[0];
     }
-
-    return (index == numCourses) ? order : new int[0];
-}
 }
