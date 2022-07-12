ab42327277d5942aea21bb1a0eb1c1fec71a5256
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-12 09:49:04 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (84.42%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,47 +14,17 @@
  * }
  */
 class Solution {
-    boolean isValid = true;
     public boolean isSymmetric(TreeNode root) {
-//         if(root == null)
-//             return true;
-//         if(root.left == null && root.right != null)
-//             return false;
-//         if(root.left != null && root.right == null)
-//             return false;
-//         Deque<TreeNode> queue  = new LinkedList<TreeNode>();
-//         queue.offer(root.left);
-//         queue.offer(root.right);
-//         while(!queue.isEmpty()){
-//             TreeNode t1 = queue.poll();
-//             TreeNode t2 = queue.poll();
-//             if(t1 == null && t2 == null)
-//                 continue;
-//             if((t1 == null && t2 != null) || (t1 != null && t2 == null))
-//                 return false;
-//             if(t1.val != t2.val)
-//                 return false;
-
-//             queue.offer(t1.left);
-//             queue.offer(t2.right);
-//             queue.offer(t1.right);
-//             queue.offer(t2.left);
-//         }
-        
-//         return true;
         return helper(root.left, root.right);
-        
     }
-    boolean helper(TreeNode r1, TreeNode r2){
-        if(r1 == null && r2 == null)
+    
+    boolean helper(TreeNode p, TreeNode q){
+        if(p == null && q == null)
             return true;
-        if(r1 == null && r2 != null)
+        else if((p == null && q != null) || (p != null && q == null))
             return false;
-        if(r1 != null && r2 == null)
+        else if(p.val != q.val)
             return false;
-        if(r1.val == r2.val)
-            return helper(r1.left, r2.right) && helper(r1.right, r2.left);
-        return false;
-            
+        return helper(p.left, q.right) && helper(p.right, q.left);
     }
 }
