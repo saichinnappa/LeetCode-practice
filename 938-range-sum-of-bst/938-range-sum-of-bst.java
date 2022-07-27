1dc3bb62ddd11b3c14c9f09f71a6779e15704d47
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-07-27 08:21:13 -0500

    Time: 2 ms (39.06%), Space: 67.4 MB (29.39%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -16,43 +16,16 @@
 class Solution {
     int result = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
-        
-        //APPROACH 1: Recursive
-        // First check the root value with low and hight
-        // case 1: if it falls in between, the start traversing from the root.
-        // case 2: if high <= root, then traverse only left of the tree.
-        // case 3: if low >= root, then traverse only right of the tree;
-        
-
-        
-        if(root == null)
-            return 0;
         helper(root, low, high);
-        // if( low <= root.val && root.val <= high){
-        //     helper(root, low, high);
-        // } else if (low >= root.val){
-        //     if(low == root.val)
-        //         result += root.val;
-        //     helper(root.right, low, high);
-        // } else if(high <= root.val){
-        //     if(high == root.val){
-        //         result += root.val;
-        //     }
-        //     helper(root.left, low, high);
-        // }
-        
         return result;
     }
     
     void helper(TreeNode root, int low, int high){
-
         if(root == null)
             return;
-        if( low <= root.val && root.val <= high)
-            result += root.val;
-        
         helper(root.left, low, high);
+        if(root.val >= low && root.val <= high)
+            result += root.val;
         helper(root.right, low, high);
-        
     }
 }
