7d4e07fc0176a47deb17f28e022fd93e6b033a0c
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-03 13:29:40 -0500

    Time: 0 ms (100.00%), Space: 42.8 MB (48.00%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,24 +1,30 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // boolean[][] track = new boolean[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = i + 1; j < matrix[0].length; j++){
-                // if(i != j && !track[i][j]){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && !visited[i][j] && !visited[i][j]){
+                    visited[i][j] = true;
+                    visited[j][i] = true;
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
-                    // track[i][j] = true;
-                    // track[j][i] = true;
-                // }
+                }
             }
         }
-        int n = matrix[0].length - 1;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length / 2 ; j++){
+        
+        // System.out.println(Arrays.deepToString(matrix));
+        
+        int k = n - 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n/2; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - j];
-                matrix[i][n - j] = temp;
+                matrix[i][j] = matrix[i][k - j];
+                matrix[i][k - j] = temp;
             }
         }
+        
     }
 }
