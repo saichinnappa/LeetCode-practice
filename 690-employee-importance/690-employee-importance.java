8c93574b40dd17a00047588e8ad20e34529bb6e7
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-05 09:09:17 -0500

    Time: 10 ms (46.36%), Space: 58.1 MB (68.40%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.java
@@ -0,0 +1,35 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    int result = 0;
+    Map<Integer, Employee> map = new HashMap<>();
+    public int getImportance(List<Employee> employees, int id) {
+
+        for(Employee e: employees){
+            map.put(e.id, e);
+        }
+        
+        if(!map.containsKey(id))
+            return 0;
+        
+        helper(map.get(id));
+        return result;  
+    }
+    
+    void helper(Employee e){
+        result += e.importance;
+        if(e.subordinates.size() > 0){
+            for(int e1 : e.subordinates){
+                helper(map.get(e1));
+            }    
+        }
+        
+    }
+}
