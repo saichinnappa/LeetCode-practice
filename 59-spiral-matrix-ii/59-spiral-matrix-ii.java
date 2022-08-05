a7169770376deac4d98a3ffaa0481d306db14121
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-04 20:10:57 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (33.53%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int x = 1;
+        int[][] result = new int[n][n];
+        
+        int top = 0;
+        int left = 0;
+        int right = n - 1;
+        int bottom = n - 1;
+        
+        while(x != (n*n) + 1){
+            
+            //top
+            for(int i = left; i <= right; i++){
+                result[top][i] = x++;
+            }
+            top++;
+            // System.out.println(Arrays.deepToString(result));
+            
+            //right
+            for(int i = top; i <= bottom; i++){
+                result[i][right] = x++;
+            }
+            right--;
+            // System.out.println(Arrays.deepToString(result));
+            
+            //bottom
+            for(int i = right; i >= left; i--){
+                result[bottom][i] = x++;
+            }
+            bottom--;
+             // System.out.println(Arrays.deepToString(result));
+            //left
+            for(int i = bottom; i >= top; i--){
+                result[i][left] = x++;
+            }
+            left++;
+            // System.out.println(Arrays.deepToString(result));
+            // System.out.println(x);
+            
+        }
+        return result;
+    }
+}
