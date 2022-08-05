57a64cb7e76d87642e088a67c6aa6c62b88652fd
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-05 08:18:41 -0500

    Time: 19 ms (54.19%), Space: 50.9 MB (33.68%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,5 +1,60 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    int[] nums;
+    Map<Integer, Integer> map = new HashMap<>();
+    public int[] topKFrequent(int[] num, int k) {
+        
+        for(int n1 : num){
+            map.put(n1, map.getOrDefault(n1, 0) + 1);
+        }
+        
+        this.nums = new int[map.size()];
+        int i = 0;
+        for(int n : map.keySet()){
+            nums[i] = n;
+            i++;
+        }
+        int n = this.nums.length;
+        quickselect(n - k, 0, nums.length - 1);
+        return Arrays.copyOfRange(this.nums, n - k, n);
+        
+    }
+    
+    void quickselect(int k, int left, int right){
+        if(left == right)
+            return;
+        
+        int pivot = partition(left, right);
+        if(pivot == k){
+           return;
+            
+           
+        } else if(pivot > k){
+         quickselect(k, left, pivot - 1);
+        }else
+        
+         quickselect(k, pivot + 1, right);
+        
+    }
+    
+    
+    int partition(int start, int end){
+        int partitionIndex = start - 1;
+        int pivot = map.get(this.nums[end]);
+        for(int i = start; i <= end; i++){
+            if(map.get(this.nums[i]) <= pivot){
+                partitionIndex++;
+                int temp = this.nums[partitionIndex];
+                this.nums[partitionIndex] = this.nums[i];
+                this.nums[i] = temp;
+            }
+        }
+        return partitionIndex;
+    }
+    
+    
+    
+    
+    /*public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Pair> map = new HashMap();
         for(int i : nums){
             if(!map.containsKey(i))
@@ -43,5 +98,5 @@ public class myComparator implements Comparator<Pair>{
             return 1;
         else
             return 0;
-    }
+    } */
 }
