613c7547de90059400b818c3ffd130d78637772d
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-17 15:24:32 -0500

    Time: 12 ms (16.83%), Space: 56.4 MB (76.57%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,8 +2,9 @@ class Solution {
     char[][] grid;
     int m, n;
     int result = 0;
+    int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public int numIslands(char[][] grid) {
-        
+        Deque<int[]>queue = new LinkedList<>();
         this.grid = grid;
         this.m = grid.length;
         this.n = grid[0].length;
@@ -11,7 +12,18 @@ class Solution {
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
                     result++;
-                    dfs(i, j);
+                    queue.offer(new int[]{i, j});
+                    while(!queue.isEmpty()){
+                        int[] first = queue.poll();
+                        for(int[] dir : dirs){
+                            int row = first[0] + dir[0];
+                            int col = first[1] + dir[1];
+                            if(row > -1 && col > -1 && row < m && col < n && grid[row][col] == '1'){
+                                grid[row][col] = 'X';
+                                queue.offer(new int[]{row,col});
+                            }
+                        }
+                    }
                 }
             }
         }
