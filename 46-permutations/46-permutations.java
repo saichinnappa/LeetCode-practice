dbf69315ef74a92d14db00bd12b78444bf445c70
Sai Chinnappa <chinnappa.sai@gmail.com>
2022-08-17 16:13:36 -0500

    Time: 8 ms (5.68%), Space: 45.4 MB (11.81%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,74 +1,35 @@
-// class Solution {
-//     List<List<Integer>> result = new ArrayList<>();
-//     int[] nums;
-//     Set<Integer> set = new HashSet<>();
-//     public List<List<Integer>> permute(int[] nums) {
-//         this.nums = nums;
-//         solve(new ArrayList<>());
-//         return result;
-//     }
-    
-    
-//     List<Integer> getCandidates(){
-//         List<Integer> candidates = new ArrayList<>();
-//         for(int n : nums){
-//             if(!set.contains(n)){
-//                 candidates.add(n);
-//             }
-//         }
-        
-//         return candidates;
-//     }
-    
-//     void solve(List<Integer> list){
-//         if(list.size() == nums.length){
-//             result.add(new ArrayList<>(list));
-//             return;
-//         }
-//         List<Integer> candidates = getCandidates();
-//         for(int n : candidates){
-//             set.add(n);
-//             list.add(n);
-//             solve(list);
-//             set.remove(n);
-//             System.out.println("Removing: "+ list.get(list.size() - 1));
-//             list.remove(list.size() - 1);
-//         }
-//     }
-// }
-
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    Set<Integer> set = new HashSet<>();
     public List<List<Integer>> permute(int[] nums) {
-        LinkedList<Integer> _nums = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
-            _nums.add(nums[i]);
-        }
-        return permutation(_nums);
-
+        int n = nums.length;
+        solve(0, new ArrayList<>(), nums);
+        return result;
     }
-
-    public List<List<Integer>> permutation(LinkedList<Integer> nums) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        if (nums.size() == 1) {
-            res.add(new ArrayList<>(nums));
-            return res;
+    
+    List<Integer> getCandidates(int[] nums){
+        List<Integer> list = new ArrayList<>();
+        for(int n : nums){
+            if(!set.contains(n))
+                list.add(n);
         }
-
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums.pollFirst();
-
-            List<List<Integer>> permutations = permutation(nums);
-            for (List<Integer> perm : permutations) {
-                perm.add(num);
-            }
-
-            res.addAll(permutations);
-
-            nums.addLast(num);
-
-        }
-
-        return res;
+        return list;
     }
+    
+    
+    void solve(int idx, List<Integer> list, int[] nums){
+        if(list.size() == nums.length){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        List<Integer> candidates = getCandidates(nums);
+        for(int c : candidates){
+            list.add(c);
+            set.add(c);
+            solve(idx, list, nums);
+            set.remove(c);
+            list.remove(list.size() - 1);
+        }
+    }
+    
 }
